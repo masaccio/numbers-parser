2b3c17d74b24a6ba33a8cd579063af72820670e0
Peter Sobot <213293+psobot@users.noreply.github.com>
2019-02-04 22:38:29 -0500

    Added missing requirements.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.3',
+    version='1.0.4',
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -130,6 +130,7 @@ setup(
         'tqdm>=4.14.0',
         'python-snappy>=0.5.3',
         'PyYAML>=4.2b1',
+        'Pillow==5.4.1',
     ],
 
     # List additional groups of dependencies here (e.g. development
