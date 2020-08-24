ffd6a6c2d6691621b1f0eef008ed54237433e24a
Peter Sobot <github@petersobot.com>
2020-08-24 07:12:29 -0400

    Added more verbose error logging.

--- keynote_parser/__init__.py
+++ keynote_parser/__init__.py
@@ -5,7 +5,7 @@ __author__ = "Peter Sobot"
 import keynote_parser.macos_app_version
 
 __major_version__ = 1
-__patch_version__ = 0
+__patch_version__ = 1
 __supported_keynote_version__ = keynote_parser.macos_app_version.MacOSAppVersion(
     "10.1", "6913", "1A148"
 )
