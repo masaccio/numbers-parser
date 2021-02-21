3ccb44408a5084fd76ea3b5a73e84c584c9cf4a3
Jon Connell <github@figsandfudge.com>
2021-02-21 12:23:05 +0000

    Removed ProtobufPatch fixup

--- numbers_parser/mapping.py
+++ numbers_parser/mapping.py
@@ -1,36 +1,36 @@
-from .generated import TNArchives_pb2 as TNArchives
-from .generated import TSTArchives_pb2 as TSTArchives
-from .generated import TSCH3DArchives_pb2 as TSCH3DArchives
-from .generated import TSWPCommandArchives_sos_pb2 as TSWPCommandArchives_sos
-from .generated import TSSArchives_pb2 as TSSArchives
-from .generated import TSAArchives_sos_pb2 as TSAArchives_sos
-from .generated import TSCHArchives_GEN_pb2 as TSCHArchives_GEN
-from .generated import TSTStylePropertyArchiving_pb2 as TSTStylePropertyArchiving
-from .generated import TSPMessages_pb2 as TSPMessages
-from .generated import TSDCommandArchives_pb2 as TSDCommandArchives
-from .generated import TSCHPreUFFArchives_pb2 as TSCHPreUFFArchives
-from .generated import TSWPCommandArchives_pb2 as TSWPCommandArchives
-from .generated import TSPDatabaseMessages_pb2 as TSPDatabaseMessages
-from .generated import TSSArchives_sos_pb2 as TSSArchives_sos
-from .generated import TSDArchives_sos_pb2 as TSDArchives_sos
-from .generated import TNCommandArchives_pb2 as TNCommandArchives
-from .generated import TSPArchiveMessages_pb2 as TSPArchiveMessages
-from .generated import TSCHCommandArchives_pb2 as TSCHCommandArchives
-from .generated import TSWPArchives_pb2 as TSWPArchives
-from .generated import TSWPArchives_sos_pb2 as TSWPArchives_sos
-from .generated import TNCommandArchives_sos_pb2 as TNCommandArchives_sos
-from .generated import TSKArchives_sos_pb2 as TSKArchives_sos
-from .generated import TSCHArchives_sos_pb2 as TSCHArchives_sos
-from .generated import TSDArchives_pb2 as TSDArchives
-from .generated import TNArchives_sos_pb2 as TNArchives_sos
-from .generated import TSKArchives_pb2 as TSKArchives
-from .generated import TSTCommandArchives_pb2 as TSTCommandArchives
-from .generated import TSCHArchives_pb2 as TSCHArchives
-from .generated import TSACommandArchives_sos_pb2 as TSACommandArchives_sos
-from .generated import TSTArchives_sos_pb2 as TSTArchives_sos
-from .generated import TSAArchives_pb2 as TSAArchives
-from .generated import TSCEArchives_pb2 as TSCEArchives
-from .generated import TSCHArchives_Common_pb2 as TSCHArchives_Common
+from numbers_parser.generated import TNArchives_pb2 as TNArchives
+from numbers_parser.generated import TSTArchives_pb2 as TSTArchives
+from numbers_parser.generated import TSCH3DArchives_pb2 as TSCH3DArchives
+from numbers_parser.generated import TSWPCommandArchives_sos_pb2 as TSWPCommandArchives_sos
+from numbers_parser.generated import TSSArchives_pb2 as TSSArchives
+from numbers_parser.generated import TSAArchives_sos_pb2 as TSAArchives_sos
+from numbers_parser.generated import TSCHArchives_GEN_pb2 as TSCHArchives_GEN
+from numbers_parser.generated import TSTStylePropertyArchiving_pb2 as TSTStylePropertyArchiving
+from numbers_parser.generated import TSPMessages_pb2 as TSPMessages
+from numbers_parser.generated import TSDCommandArchives_pb2 as TSDCommandArchives
+from numbers_parser.generated import TSCHPreUFFArchives_pb2 as TSCHPreUFFArchives
+from numbers_parser.generated import TSWPCommandArchives_pb2 as TSWPCommandArchives
+from numbers_parser.generated import TSPDatabaseMessages_pb2 as TSPDatabaseMessages
+from numbers_parser.generated import TSSArchives_sos_pb2 as TSSArchives_sos
+from numbers_parser.generated import TSDArchives_sos_pb2 as TSDArchives_sos
+from numbers_parser.generated import TNCommandArchives_pb2 as TNCommandArchives
+from numbers_parser.generated import TSPArchiveMessages_pb2 as TSPArchiveMessages
+from numbers_parser.generated import TSCHCommandArchives_pb2 as TSCHCommandArchives
+from numbers_parser.generated import TSWPArchives_pb2 as TSWPArchives
+from numbers_parser.generated import TSWPArchives_sos_pb2 as TSWPArchives_sos
+from numbers_parser.generated import TNCommandArchives_sos_pb2 as TNCommandArchives_sos
+from numbers_parser.generated import TSKArchives_sos_pb2 as TSKArchives_sos
+from numbers_parser.generated import TSCHArchives_sos_pb2 as TSCHArchives_sos
+from numbers_parser.generated import TSDArchives_pb2 as TSDArchives
+from numbers_parser.generated import TNArchives_sos_pb2 as TNArchives_sos
+from numbers_parser.generated import TSKArchives_pb2 as TSKArchives
+from numbers_parser.generated import TSTCommandArchives_pb2 as TSTCommandArchives
+from numbers_parser.generated import TSCHArchives_pb2 as TSCHArchives
+from numbers_parser.generated import TSACommandArchives_sos_pb2 as TSACommandArchives_sos
+from numbers_parser.generated import TSTArchives_sos_pb2 as TSTArchives_sos
+from numbers_parser.generated import TSAArchives_pb2 as TSAArchives
+from numbers_parser.generated import TSCEArchives_pb2 as TSCEArchives
+from numbers_parser.generated import TSCHArchives_Common_pb2 as TSCHArchives_Common
 
 
 PROTO_FILES = [
@@ -613,21 +613,3 @@ TSPRegistryMapping = {
     "12053": "TN.FormBuilderSelectionArchive",
     "12054": "TN.FormTableChooserSelectionArchive",
 }
-
-
-def compute_maps():
-    name_class_map = {}
-    for file in PROTO_FILES:
-        for message_name in file.DESCRIPTOR.message_types_by_name:
-            message_type = getattr(file, message_name)
-            name_class_map[message_type.DESCRIPTOR.full_name] = message_type
-
-    id_name_map = {}
-    for k, v in list(TSPRegistryMapping.items()):
-        if v in name_class_map:
-            id_name_map[int(k)] = name_class_map[v]
-
-    return name_class_map, id_name_map
-
-
-NAME_CLASS_MAP, ID_NAME_MAP = compute_maps()
