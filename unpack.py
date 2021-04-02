dbb38b5fe4deeefa0a5ee419c98206c77c11e34c
Jon Connell <github@figsandfudge.com>
2021-04-02 17:25:44 +0100

    File I/O just for unpack utility

--- unpack.py
+++ unpack.py
@@ -1,24 +1,117 @@
 import argparse
+import os
+import pprint
+import sys
 
-import numbers_parser
+from contextlib import contextmanager
+from glob import glob
+from numbers_parser.codec import IWAFile
+from numbers_parser.unicode_utils import fix_unicode
+from zipfile import ZipFile
 
-numbers_parser.__command_line_invocation__ = True  # noqa
 
-from numbers_parser import __version__, __supported_numbers_version__
-from numbers_parser.bundle_utils import (
-    warn_once_on_newer_numbers,
-    get_installed_numbers_version,
-)
-from numbers_parser.file_utils import process
+def ensure_directory_exists(prefix, path):
+    """Ensure that a path's directory exists."""
+    parts = os.path.split(path)
+    try:
+        os.makedirs(os.path.join(*([prefix] + list(parts[:-1]))))
+    except OSError:
+        pass
+
+
+def file_reader(path):
+    if not os.path.isdir(path) and path.endswith(".numbers"):
+        return zip_file_reader(path)
+    else:
+        return directory_reader(path)
+
+
+def zip_file_reader(path):
+    zipfile = ZipFile(path, "r")
+    for _file in zipfile.filelist:
+        _file.filename = _file.filename.encode("cp437").decode("utf-8")
+    iterator = sorted(zipfile.filelist, key=lambda x: x.filename)
+    for zipinfo in iterator:
+        if zipinfo.filename.endswith("/"):
+            continue
+        with zipfile.open(zipinfo) as handle:
+            yield (zipinfo.filename, handle)
+
+
+def directory_reader(path):
+    # Python <3.5 doesn't support glob with recursive, so this will have to do.
+    iterator = set(sum([glob(path + ("/**" * i)) for i in range(10)], []))
+    iterator = sorted(iterator)
+    for filename in iterator:
+        if os.path.isdir(filename):
+            continue
+        rel_filename = filename.replace(path + "/", "")
+        with open(filename, "rb") as handle:
+            yield (rel_filename, handle)
+
+
+@contextmanager
+def dir_file_sink(target_dir):
+    def accept(filename, contents):
+        ensure_directory_exists(target_dir, filename)
+        target_path = os.path.join(target_dir, filename)
+        if isinstance(contents, IWAFile):
+            target_path += ".txt"
+            with open(target_path, "w") as out:
+                pprint.PrettyPrinter(indent=2, stream=out).pprint(contents.to_dict())
+        else:
+            with open(target_path, "wb") as out:
+                if isinstance(contents, IWAFile):
+                    out.write(contents.to_buffer())
+                else:
+                    out.write(contents)
+
+    yield accept
+
+
+def process_file(filename, handle, sink):
+    contents = None
+    if ".iwa" in filename:
+        contents = handle.read()
+        if filename.endswith(".yaml"):
+            file = IWAFile.from_dict(
+                yaml.load(fix_unicode(contents.decode("utf-8")), Loader=Loader)
+            )
+            filename = filename.replace(".yaml", "")
+        else:
+            file = IWAFile.from_buffer(contents, filename)
+
+        file_has_changed = False
+
+        if file_has_changed:
+            data = file.to_dict()
+            sink(filename, IWAFile.from_dict(data))
+        else:
+            sink(filename, file)
+        return
+
+    if filename.startswith("Data/"):
+        file_has_changed = False
+
+        if file_has_changed:
+            return
+    sink(filename, contents or handle.read())
+
+
+def process(input_path, output_path, subfile=None):
+    with dir_file_sink(output_path) as sink:
+        for filename, handle in file_reader(input_path):
+            try:
+                process_file(filename, handle, sink)
+            except Exception as e:
+                raise ValueError("Failed to process file %s due to: %s" % (filename, e))
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-v", "--version", action="version", version=__version__)
     parser.add_argument("input", help="a .numbers file")
     parser.add_argument("--output", "-o", help="a directory name to unpack into")
     args = parser.parse_args()
-    warn_once_on_newer_numbers()
     process(args.input, args.output or args.input.replace(".numbers", ""))
 
 
