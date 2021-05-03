5f9e4f36cf0b65dca48adcd150fe4d0873337fd8
Jon Connell <github@figsandfudge.com>
2021-05-03 21:43:38 +0100

    Reformatting and support of --brief

--- catnumbers.py
+++ catnumbers.py
@@ -20,6 +20,13 @@ def command_arguments():
         action="store_true",
         help="List the names of sheets and exit",
     )
+    commands.add_argument(
+        "-b",
+        "--brief",
+        action="store_true",
+        default=False,
+        help="Don't prefix data rows with name of sheet/table (default: false)",
+    )
     parser.add_argument(
         "-s", "--sheet", nargs="*", help="Names of sheet(s) to include in export"
     )
@@ -30,30 +37,33 @@ def command_arguments():
     return parser.parse_args()
 
 
-def print_sheet_names(filenames):
-    for filename in filenames:
-        for sheet in Document(filename).sheets():
-            print(f"{filename}: {sheet.name}")
+def print_sheet_names(filename):
+    for sheet in Document(filename).sheets():
+        print(f"{filename}: {sheet.name}")
 
 
-def print_table_names(filenames):
-    for filename in filenames:
-        for sheet in Document(filename).sheets():
-            for table in sheet.tables():
-                print(f"{filename}: {sheet.name}: {table.name}")
+def print_table_names(filename):
+    for sheet in Document(filename).sheets():
+        for table in sheet.tables():
+            print(f"{filename}: {sheet.name}: {table.name}")
+
+
+def print_table(filename):
+    for sheet in Document(filename).sheets():
+        for table in sheet.tables():
+            for row in table.data:
+                cols = ",".join([str(s) if s is not None else "" for s in row])
+                if args.brief:
+                    print(cols)
+                else:
+                    print(f"{filename}: {sheet.name}: {table.name}:", cols)
 
 
 args = command_arguments()
-if args.list_sheets:
-    print_sheet_names(args.document)
-elif args.list_tables:
-    print_table_names(args.document)
-else:
-    for filename in args.document:
-        for sheet in Document(filename).sheets():
-            for table in sheet.tables():
-                for row in table.data:
-                    print(
-                        f"{filename}: {sheet.name}: {table.name}:",
-                        ",".join([str(s) if s is not None else "" for s in row]),
-                    )
+for filename in args.document:
+    if args.list_sheets:
+        print_sheet_names(filename)
+    elif args.list_tables:
+        print_table_names(filename)
+    else:
+        print_table(filename)
