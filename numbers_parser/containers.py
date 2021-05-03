0ded3ccc3bd94e064c5d3731d2428a33f5a572b8
Jon Connell <github@figsandfudge.com>
2021-05-03 13:38:08 +0100

    Added support for Numbers spreadsheets stored as folders

--- numbers_parser/containers.py
+++ numbers_parser/containers.py
@@ -1,5 +1,9 @@
 from zipfile import ZipFile, BadZipFile
 from numbers_parser.codec import IWAFile
+from pathlib import Path
+
+import os
+
 
 class NumbersError(Exception):
     """Base class for other exceptions"""
@@ -44,36 +48,23 @@ class ItemsList:
 
 class ObjectStore:
     def __init__(self, filename):
-        try:
-            zipf = ZipFile(filename)
-        except BadZipFile as e:
-            raise FileError(f"{filename}: " + str(e))
-
         objects = {}
-        iwa_files = filter(lambda x: x.endswith(".iwa"), zipf.namelist())
-        for iwa_filename in iwa_files:
-            contents = zipf.read(iwa_filename)
-
+        if os.path.isdir(filename):
+            iwa_files = list(Path(filename).rglob("*.iwa"))
+            for iwa_filename in iwa_files:
+                f = open(iwa_filename, "rb")
+                contents = f.read()
+                objects.update(extract_iwa_archives(contents, iwa_filename))
+        else:
             try:
-                iwaf = IWAFile.from_buffer(contents, iwa_filename)
-            except Error as e:
-                raise FileFormatError(f"{filename}: invalid IWA file {iwa_filename}") from e
+                zipf = ZipFile(filename)
+            except BadZipFile as e:
+                raise FileError(f"{filename}: " + str(e))
 
-            if len(iwaf.chunks) != 1:
-                raise FileFormatError(f"{filename}: chunk count != 1 in {iwa_filename}")
-            for archive in iwaf.chunks[0].archives:
-                if len(archive.objects) == 0:
-                    raise FileFormatError(f"{filename}: no objects in {iwa_filename}")
-
-                identifier = archive.header.identifier
-                if identifier in objects:
-                    raise FileFormatError(f"{filename}: duplicate reference {identifier}")
-
-                if len(archive.objects) == 1:
-                    objects[identifier] = archive.objects[0]
-                else:
-                    # print(f"warning: {iwa_filename}: found", len(archive.objects), "objects")
-                    pass
+            iwa_files = filter(lambda x: x.endswith(".iwa"), zipf.namelist())
+            for iwa_filename in iwa_files:
+                contents = zipf.read(iwa_filename)
+                objects.update(extract_iwa_archives(contents, iwa_filename))
 
         self._object_store = objects
 
@@ -84,10 +75,38 @@ class ObjectStore:
         return len(self._object_store)
 
     def find_refs(self, ref_name):
-        refs = [k for k, v in self._object_store.items() if type(v).__name__ == ref_name]
+        refs = [
+            k for k, v in self._object_store.items() if type(v).__name__ == ref_name
+        ]
         return refs
 
     def find_objects(self, ref_name, class_name):
         refs = self.find_refs(ref_name)
         class_ = getattr(importlib.import_module(__name__), class_name)
         return [class_(self, obj_id) for obj_id in refs]
+
+
+def extract_iwa_archives(contents, iwa_filename):
+    objects = {}
+    try:
+        iwaf = IWAFile.from_buffer(contents, iwa_filename)
+    except Exception as e:
+        raise FileFormatError(f"{filename}: invalid IWA file {iwa_filename}") from e
+
+    if len(iwaf.chunks) != 1:
+        raise FileFormatError(f"{filename}: chunk count != 1 in {iwa_filename}")
+    for archive in iwaf.chunks[0].archives:
+        if len(archive.objects) == 0:
+            raise FileFormatError(f"{filename}: no objects in {iwa_filename}")
+
+        identifier = archive.header.identifier
+        if identifier in objects:
+            raise FileFormatError(f"{filename}: duplicate reference {identifier}")
+
+        if len(archive.objects) == 1:
+            objects[identifier] = archive.objects[0]
+        else:
+            #  print(f"warning: {iwa_filename}: found", len(archive.objects), "objects")
+            pass
+
+    return objects
