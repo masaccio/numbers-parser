7b59efecac7689b498290d7b3484d0713164b32c
Jon Connell <github@figsandfudge.com>
2021-05-15 18:06:54 +0100

    Test allows Numbers not installed

--- tests/test_version.py
+++ tests/test_version.py
@@ -5,7 +5,7 @@ import numbers_parser
 numbers_parser._SUPPORTED_NUMBERS_VERSIONS = ["0.0"]
 
 def test_version(capsys):
-    if open(os.path.isfile(numbers_parser._DEFAULT_NUMBERS_INSTALL_PATH)):
+    if os.path.isfile(numbers_parser._DEFAULT_NUMBERS_INSTALL_PATH):
         with pytest.warns(UserWarning) as record:
             r = numbers_parser._check_installed_numbers_version()
         assert r is None or "not tested with this version" in record[0].message.args[0]
