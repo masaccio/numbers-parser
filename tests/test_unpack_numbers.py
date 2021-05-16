aed69ee455a5c42cd6fcbde41490069b0a76a25c
Jon Connell <github@figsandfudge.com>
2021-05-16 16:35:48 +0100

    Unpack document to file tree test

--- tests/test_unpack_numbers.py
+++ tests/test_unpack_numbers.py
@@ -1,8 +1,9 @@
+import json
+import imghdr
 import pytest
 
 from numbers_parser import __version__
 
-DOCUMENT = "tests/data/test-1.numbers"
 
 def test_version(script_runner):
     ret = script_runner.run("unpack-numbers", "--version", print_result=False)
@@ -24,3 +25,27 @@ def test_multi_doc_error(script_runner):
     assert ret.success == False
     assert ret.stdout == ""
     assert "output directory only valid" in ret.stderr
+
+
+def test_create_file(script_runner, tmp_path):
+    output_dir = tmp_path / "test"
+    ret = script_runner.run(
+        "unpack-numbers",
+        "--output",
+        str(output_dir),
+        "tests/data/test-1.numbers",
+        print_result=False,
+    )
+    assert ret.success
+    assert ret.stdout == ""
+    assert (output_dir / "preview.jpg").exists()
+    assert imghdr.what(str(output_dir / "preview.jpg")) == "jpeg"
+    assert (output_dir / "Index/CalculationEngine.txt").exists()
+    assert (output_dir / "Index/Tables/DataList-954857.txt").exists()
+    with open(str(output_dir / "Index/Tables/DataList-954857.txt")) as f:
+        data = json.load(f)
+    objects = data["chunks"][0]["archives"][0]["objects"]
+    strings = [x["string"] for x in objects[0]["entries"]]
+    assert len(strings) == 15
+    assert "ZZZ_2_3" in strings
+    assert "ZZZ_ROW_3" in strings
