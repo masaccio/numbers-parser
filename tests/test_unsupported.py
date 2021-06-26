9bc6dbfbdd78829853f82c8d1789c1cd3e12b18b
Jon Connell <github@figsandfudge.com>
2021-06-26 17:54:30 +0100

    cell() now returns a typed cell object

--- tests/test_unsupported.py
+++ tests/test_unsupported.py
@@ -1,10 +1,10 @@
 import pytest
 
-from numbers_parser.document import Document
+from numbers_parser import Document, FormulaCell, ErrorCell
 
 def test_table_unsupported():
     doc = Document("tests/data/test-8.numbers")
     sheets = doc.sheets()
     tables = sheets[0].tables()
-    assert tables[0].cell(1, 5) == "*FORMULA*"
-    assert tables[0].cell(3, 2) == "*ERROR*"
+    assert isinstance(tables[0].cell(1, 5), FormulaCell)
+    assert isinstance(tables[0].cell(3, 2), ErrorCell)
