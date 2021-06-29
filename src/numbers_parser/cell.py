1dbcab9c30f8bc21fbfc0d2a9e2db5c08535ff4e
Jon Connell <github@figsandfudge.com>
2021-06-29 14:26:26 +0100

    New helpers

--- src/numbers_parser/cell.py
+++ src/numbers_parser/cell.py
@@ -1,3 +1,5 @@
+import re
+
 from numbers_parser.containers import NumbersError
 from datetime import datetime, timedelta
 
@@ -59,10 +61,127 @@ class ErrorCell(Cell):
 
 
 class MergedCell:
-    def __init__(self, row_start: int, row_end: int, col_start: int, col_end: int):
+    def __init__(self, row_start: int, col_start:int, row_end: int, col_end: int):
         self.value = None
         self.row_start = row_start
         self.row_end = row_end
         self.col_start = col_start
         self.col_end = col_end
-        pass
+        self.merge_range = xl_range(row_start, col_start, row_end, col_end)
+
+
+# Cell reference conversion from  https://github.com/jmcnamara/XlsxWriter
+# Copyright (c) 2013-2021, John McNamara <jmcnamara@cpan.org>
+range_parts = re.compile(r"(\$?)([A-Z]{1,3})(\$?)(\d+)")
+
+
+def xl_cell_to_rowcol(cell_str: str) -> tuple:
+    """
+    Convert a cell reference in A1 notation to a zero indexed row and column.
+    Args:
+        cell_str:  A1 style string.
+    Returns:
+        row, col: Zero indexed cell row and column indices.
+    """
+    if not cell_str:
+        return 0, 0
+
+    match = range_parts.match(cell_str)
+    if not match:
+        raise IndexError(f"invalid cell reference {cell_str}")
+
+    col_str = match.group(2)
+    row_str = match.group(4)
+
+    # Convert base26 column string to number.
+    expn = 0
+    col = 0
+    for char in reversed(col_str):
+        col += (ord(char) - ord("A") + 1) * (26 ** expn)
+        expn += 1
+
+    # Convert 1-index to zero-index
+    row = int(row_str) - 1
+    col -= 1
+
+    return row, col
+
+
+def xl_range(first_row, first_col, last_row, last_col):
+    """
+    Convert zero indexed row and col cell references to a A1:B1 range string.
+    Args:
+       first_row: The first cell row.    Int.
+       first_col: The first cell column. Int.
+       last_row:  The last cell row.     Int.
+       last_col:  The last cell column.  Int.
+    Returns:
+        A1:B1 style range string.
+    """
+    range1 = xl_rowcol_to_cell(first_row, first_col)
+    range2 = xl_rowcol_to_cell(last_row, last_col)
+
+    if range1 == range2:
+        return range1
+    else:
+        return range1 + ":" + range2
+
+
+def xl_rowcol_to_cell(row, col, row_abs=False, col_abs=False):
+    """
+    Convert a zero indexed row and column cell reference to a A1 style string.
+    Args:
+       row:     The cell row.    Int.
+       col:     The cell column. Int.
+       row_abs: Optional flag to make the row absolute.    Bool.
+       col_abs: Optional flag to make the column absolute. Bool.
+    Returns:
+        A1 style string.
+    """
+    if row < 0:
+        raise IndexError(f"Row reference {row} below zero")
+
+    if col < 0:
+        raise IndexError(f"column reference {col} below zero")
+
+    row += 1  # Change to 1-index.
+    row_abs = "$" if row_abs else ""
+
+    col_str = xl_col_to_name(col, col_abs)
+
+    return col_str + row_abs + str(row)
+
+def xl_col_to_name(col, col_abs=False):
+    """
+    Convert a zero indexed column cell reference to a string.
+    Args:
+       col:     The cell column. Int.
+       col_abs: Optional flag to make the column absolute. Bool.
+    Returns:
+        Column style string.
+    """
+    col_num = col
+    if col_num < 0:
+        raise IndexError(f"Column reference {col_num} below zero")
+
+    col_num += 1  # Change to 1-index.
+    col_str = ''
+    col_abs = '$' if col_abs else ''
+
+    while col_num:
+        # Set remainder from 1 .. 26
+        remainder = col_num % 26
+
+        if remainder == 0:
+            remainder = 26
+
+        # Convert the remainder to a character.
+        col_letter = chr(ord('A') + remainder - 1)
+
+        # Accumulate the column letters, right to left.
+        col_str = col_letter + col_str
+
+        # Get the next order of magnitude.
+        col_num = int((col_num - 1) / 26)
+
+    return col_abs + col_str
