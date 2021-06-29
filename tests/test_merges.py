958a3fbd8663cc3bbf06e119e5fa121236b4f387
Jon Connell <github@figsandfudge.com>
2021-06-29 14:25:33 +0100

    Test extracted merge ranges

--- tests/test_merges.py
+++ tests/test_merges.py
@@ -25,12 +25,14 @@ XXX_TABLE_1_CLASSES = [
     ["TextCell", "MergedCell", "TextCell", "MergedCell", "MergedCell"],
 ]
 
+
 def test_table_contents():
     doc = Document("tests/data/test-9.numbers")
     sheets = doc.sheets()
     tables = sheets[0].tables()
     data = tables[0].data
     assert data == XXX_TABLE_1_REF
+
 
 def test_cell_classes():
     doc = Document("tests/data/test-9.numbers")
@@ -40,3 +42,35 @@ def test_cell_classes():
     for row in tables[0].iter_rows():
         data.append([type(c).__name__ for c in row])
     assert data == XXX_TABLE_1_CLASSES
+
+
+def test_merge_references():
+    doc = Document("tests/data/test-9.numbers")
+    sheets = doc.sheets()
+    table = sheets[0].tables()[0]
+    assert table.cell("B2").merge_range == "A2:B2"
+    assert table.cell("C5").merge_range == "B5:E5"
+    assert table.cell("B2").size == None
+    assert table.cell("C5").size == None
+    assert table.cell("C5").row_start == 4
+    assert table.cell("C5").row_end == 4
+    assert table.cell("C5").col_start == 1
+    assert table.cell("C5").col_end == 4
+    assert table.cell("A2").is_merged
+    assert table.cell("C1").is_merged == False
+    assert table.cell("D7").size == (2, 2)
+
+    table = sheets[1].tables()[0]
+    assert table.cell("A1").is_merged
+    assert table.cell("A1").size == (1, 2)
+    assert table.cell("B4").is_merged
+    assert table.cell("B4").size == (2, 2)
+
+
+def test_all_merged_ranges():
+    doc = Document("tests/data/test-9.numbers")
+    sheets = doc.sheets()
+    table = sheets[0].tables()[0]
+    assert table.merge_ranges == ["A2:B2", "B5:E5", "B6:B8", "C4:E4", "D7:E8"]
+    table = sheets[1].tables()[0]
+    assert table.merge_ranges == ["A1:B1", "B4:C5"]
