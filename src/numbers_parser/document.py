6b16e1f89270957c797c99ec547cd262efec5795
Jon Connell <python@figsandfudge.com>
2021-07-05 20:50:13 +0100

    Refactored experimental formulas into own module

--- src/numbers_parser/document.py
+++ src/numbers_parser/document.py
@@ -1,10 +1,13 @@
 import array
 import struct
+import binascii
 
 from typing import Union, List, Generator
 from datetime import datetime, timedelta
 
 from numbers_parser.containers import ItemsList, ObjectStore, NumbersError
+from numbers_parser.formula import TableFormulas, get_merge_cell_ranges
+
 from numbers_parser.cell import (
     BoolCell,
     Cell,
@@ -91,98 +94,6 @@ class Table:
         self._merge_cells = None
 
     @property
-    def formulas(self):
-        formula_type_lookup = {
-            k: v.name
-            for k, v in TSCEArchives._ASTNODEARRAYARCHIVE_ASTNODETYPE.values_by_number.items()
-        }
-        cell_records = _extract_cell_records(self._object_store, self._table)
-        formula_table_id = self._table.base_data_store.formula_table.identifier
-        formula_table = self._object_store[formula_table_id]
-        self._formulas = []
-        for index in range(len(formula_table.entries)):
-            formula = formula_table.entries[index]
-            row_base = cell_records[index][0]
-            col_base = cell_records[index][1]
-            ast_nodes = []
-            for node in formula.formula.AST_node_array.AST_node:
-                node_type = formula_type_lookup[node.AST_node_type]
-                if node_type == "FUNCTION_NODE":
-                    ast_nodes.append(
-                        {
-                            "type": node_type,
-                            "node_index": node.AST_function_node_index,
-                            "num_args": node.AST_function_node_numArgs,
-                        }
-                    )
-                elif node_type == "NUMBER_NODE":
-                    ast_nodes.append(
-                        {
-                            "type": node_type,
-                            "decimal_high": node.AST_number_node_decimal_high,
-                            "decimal_low": node.AST_number_node_decimal_low,
-                            "number": node.AST_number_node_number,
-                        }
-                    )
-                elif node_type == "CELL_REFERENCE_NODE":
-                    if node.AST_row.absolute:
-                        row_num = node.AST_row.row
-                    else:
-                        row_num = row_base + node.AST_row.row
-                    if node.AST_column.absolute:
-                        col_num = node.AST_column.column
-                    else:
-                        col_num = col_base + node.AST_column.column
-                    try:
-                        ref = xl_rowcol_to_cell(row_num, col_num)
-                    except IndexError:
-                        ref = "*Exception"
-
-                    ast_nodes.append(
-                        {"type": node_type, "row": row_num, "column": col_num, "ref": ref}
-                    )
-                elif node_type == "STRING_NODE":
-                    ast_nodes.append(
-                        {
-                            "type": node_type,
-                            "string": node.AST_string_node_string,
-                        }
-                    )
-                elif node_type == "PREPEND_WHITESPACE_NODE":
-                    ast_nodes.append(
-                        {
-                            "type": node_type,
-                            "whitespace": node.AST_whitespace,
-                        }
-                    )
-                elif any(
-                    [
-                        node_type == "ADDITION_NODE",
-                        node_type == "BEGIN_EMBEDDED_NODE_ARRAY",
-                        node_type == "BOOLEAN_NODE",
-                        node_type == "COLON_NODE",
-                        node_type == "CONCATENATION_NODE",
-                        node_type == "DIVISION_NODE",
-                        node_type == "END_THUNK_NODE",
-                        node_type == "EQUAL_TO_NODE",
-                        node_type == "GREATER_THAN_NODE",
-                        node_type == "MULTIPLICATION_NODE",
-                        node_type == "NOT_EQUAL_TO_NODE",
-                        node_type == "SUBTRACTION_NODE",
-                    ]
-                ):
-                    ast_nodes.append({"type": node_type})
-                else:
-                    raise UnsupportedError(
-                        f"Unsupported formula type {node_type}"
-                    )  # pragma: no cover
-
-                #  TODO: deal with error cells
-
-            self._formulas.append({"ast": ast_nodes, "row": row_base, "column": col_base})
-        return self._formulas
-
-    @property
     def data(self) -> list:
         """The data property is deprecated: use rows(values_only=True) instead"""
         return self.rows(values_only=True)
@@ -215,7 +126,7 @@ class Table:
     @property
     def merge_ranges(self) -> list:
         if self._merge_cells is None:
-            self._merge_cells = _extract_merge_cells(self._object_store, self._table)
+            self._merge_cells = get_merge_cell_ranges(self._object_store, self._table)
 
         ranges = []
         for row_num, cols in self._merge_cells.items():
@@ -225,7 +136,7 @@ class Table:
 
     def _extract_table_cells(self):
         if self._merge_cells is None:
-            self._merge_cells = _extract_merge_cells(self._object_store, self._table)
+            self._merge_cells = get_merge_cell_ranges(self._object_store, self._table)
 
         row_infos = []
         for tile in self._tiles:
@@ -254,6 +165,8 @@ class Table:
             for r in row_infos
         ]
 
+        table_formulas = TableFormulas(self._object_store, self._table)
+
         table_cells = []
         num_rows = sum([t.numrows for t in self._tiles])
         for row_num in range(num_rows):
@@ -281,6 +194,7 @@ class Table:
                     row.append(cell)
                 else:
                     cell_type = storage_buffer[1]
+
                     if cell_type == TSTArchives.emptyCellValueType:
                         cell = EmptyCell(row_num, col_num)
                     elif cell_type == TSTArchives.numberCellType:
@@ -290,8 +204,8 @@ class Table:
                             cell_value = struct.unpack("<d", storage_buffer_pre_bnc[-12:-4])[0]
                         cell = NumberCell(row_num, col_num, cell_value)
                     elif cell_type == TSTArchives.textCellType:
-                        key = struct.unpack("<i", storage_buffer[12:16])[0]
-                        cell = TextCell(row_num, col_num, self._table_string(key))
+                        string_key = struct.unpack("<i", storage_buffer[12:16])[0]
+                        cell = TextCell(row_num, col_num, self._table_string(string_key))
                     elif cell_type == TSTArchives.dateCellType:
                         if storage_buffer_pre_bnc is None:
                             cell_value = datetime(2001, 1, 1)
@@ -307,7 +221,8 @@ class Table:
                         cell = DurationCell(row_num, col_num, timedelta(days=cell_value))
                     elif cell_type == TSTArchives.formulaErrorCellType:
                         cell = ErrorCell(row_num, col_num)
-                    elif cell_type == 9:
+                    elif cell_type == TSTArchives.currencyCellValueType:
+                        cell_value = struct.unpack("<d", storage_buffer_pre_bnc[-12:-4])[0]
                         cell = FormulaCell(row_num, col_num)
                     elif cell_type == 10:
                         # TODO: Is this realy a number cell (experiments suggest so)
@@ -328,6 +243,51 @@ class Table:
                     except KeyError:
                         cell.is_merged = False
                         cell.size = (1, 1)
+
+                    if table_formulas.is_formula(row_num, col_num):
+                        try:
+                            if table_formulas.is_error(row_num, col_num):
+                                formula_key = struct.unpack("<h", storage_buffer[12:14])[0]
+                            if cell_type == TSTArchives.numberCellType:  # 2
+                                formula_key = struct.unpack("<h", storage_buffer[28:30])[0]
+                            elif cell_type == TSTArchives.textCellType:  # 3
+                                if len(storage_buffer) > 28:
+                                    formula_key = struct.unpack("<h", storage_buffer[20:22])[0]
+                                else:
+                                    formula_key = struct.unpack("<h", storage_buffer[24:26])[0]
+                            elif cell_type == TSTArchives.dateCellType:  # 5
+                                formula_key = struct.unpack("<h", storage_buffer[24:26])[0]
+                            elif cell_type == TSTArchives.boolCellType:  # 6
+                                formula_key = struct.unpack("<h", storage_buffer[24:26])[0]
+                            elif cell_type == TSTArchives.durationCellType:  # 7
+                                formula_key = struct.unpack("<h", storage_buffer[24:26])[0]
+                            elif cell_type == TSTArchives.formulaErrorCellType:  # 8
+                                formula_key = struct.unpack("<h", storage_buffer[12:14])[0]
+                            elif cell_type == TSTArchives.currencyCellValueType:  # 9
+                                formula_key = struct.unpack("<h", storage_buffer[32:34])[0]
+                            elif cell_type == 10:
+                                # Might be [36:38]
+                                formula_key = struct.unpack("<h", storage_buffer[32:34])[0]
+                            else:
+                                raise UnsupportedError(  # pragma: no cover
+                                    f"Unsupported formula cell type '{cell_type}' @{self.name}:({row_num},{col_num})"
+                                )
+
+                            # print(f"@{row_num},{col_num}: type={cell_type}, key={formula_key}")
+                            # print("\tstorage_buffer =", binascii.hexlify(storage_buffer, sep=":"))
+                            ast = table_formulas.ast(formula_key)
+                            ast["row"] = row_num
+                            ast["column"] = col_num
+                            cell.add_formula(ast)
+                        except KeyError:
+                            raise UnsupportedError(  # pragma: no cover
+                                f"Formula not found @{self.name}:({row_num},{col_num})"
+                            )
+                        except struct.error:
+                            raise UnsupportedError(  # pragma: no cover
+                                f"Unsupported formula ref @{self.name}:({row_num},{col_num})"
+                            )
+
                     row.append(cell)
             table_cells.append(row)
 
@@ -499,179 +459,3 @@ def _extract_cell_data(
         data.append(storage_buffer[start:end])
 
     return data
-
-
-def _uuid(ref: dict) -> int:
-    """
-    Extract storage buffers for each cell in a table row
-
-    Args:
-        ref: Google protobuf containing either four 32-bit IDs or two 64-bit IDs
-
-    Returns:
-        uuid: 128-bit UUID
-
-    Raises:
-        UnsupportedError: object does not include expected UUID fields
-    """
-    try:
-        uuid = ref.upper << 64 | ref.lower
-    except AttributeError:
-        try:
-            uuid = (ref.uuid_w3 << 96) | (ref.uuid_w2 << 64) | (ref.uuid_w1 << 32) | ref.uuid_w0
-        except AttributeError:
-            raise UnsupportedError(f"Unsupported UUID structure: {ref}")  # pragma: no cover
-
-    return uuid
-
-
-def _extract_merge_cells(object_store: ObjectStore, table: Table):
-    """Exract all the merge cell ranges for the Table."""
-    # Merge ranges are stored in a number of structures, but the simplest is
-    # a TSCE.RangePrecedentsTileArchive which exists for each Table in the
-    # Document. These archives contain a fromToRange list which has the merge
-    # ranges associated with an Owner ID
-    #
-    # The Owner IDs need to be extracted from teh Calculation Engine using
-    # UUID matching (this seems fragile, but no other mechanism has been found)
-    #
-    #  "fromToRange": [
-    #      {
-    #          "fromCoord": {
-    #          "column": 0,
-    #          "row": 0
-    #      },
-    #      "refersToRect": {
-    #          "origin": {
-    #              "column": 0,
-    #              "row": 0
-    #          },
-    #          "size": {
-    #              "numColumns": 2
-    #          }
-    #      }
-    #  ],
-    #  "toOwnerId": 1
-    #
-    owner_id_map = _extract_owner_id_map(object_store)
-    table_base_id = _extract_table_base_id(object_store, table)
-
-    merge_cells = {}
-    range_table_ids = object_store.find_refs("RangePrecedentsTileArchive")
-    for range_id in range_table_ids:
-        o = object_store[range_id]
-        to_owner_id = o.to_owner_id
-        if owner_id_map[to_owner_id] == table_base_id:
-            for from_to_range in o.from_to_range:
-                rect = from_to_range.refers_to_rect
-                row_start = rect.origin.row
-                row_end = row_start + rect.size.num_rows - 1
-                col_start = rect.origin.column
-                col_end = col_start + rect.size.num_columns - 1
-                for row_num in range(row_start, row_end + 1):
-                    if row_num not in merge_cells:
-                        merge_cells[row_num] = {}
-                    for col_num in range(col_start, col_end + 1):
-                        merge_cells[row_num][col_num] = {
-                            "merge_type": "ref",
-                            "rect": (row_start, col_start, row_end, col_end),
-                            "size": (rect.size.num_rows, rect.size.num_columns),
-                        }
-                merge_cells[row_start][col_start]["merge_type"] = "source"
-    return merge_cells
-
-
-def _extract_owner_id_map(object_store: ObjectStore):
-    """ "
-    Extracts the mapping table from Owner IDs to UUIDs. Returns a
-    dictionary mapping the owner ID int to a 128-bit UUID.
-    """
-    # The TSCE.CalculationEngineArchive contains a list of mapping entries
-    # in dependencyTracker.formulaOwnerDependencies from the root level
-    # of the protobuf. Each mapping contains a 32-bit style UUID:
-    #
-    # "ownerIdMap": {
-    #     "mapEntry": [
-    #     {
-    #         "internalOwnerId": 33,
-    #         "ownerId": {
-    #             "uuidW0": "0x8750e563, "uuidW1": "0x1e4bfcc0,
-    #             "uuidW2": "0xc26dda92, "uuidW3": "0x3cb03f23
-    #         }
-    #     },
-    #
-    #
-    calculation_engine_id = object_store.find_refs("CalculationEngineArchive")[0]
-    owner_id_map = {}
-    for e in object_store[calculation_engine_id].dependency_tracker.owner_id_map.map_entry:
-        owner_id_map[e.internal_owner_id] = _uuid(e.owner_id)
-    return owner_id_map
-
-
-def _extract_table_base_id(object_store: ObjectStore, table: Table):
-    """"Finds the UUID of a table"""
-    # Look for a TSCE.FormulaOwnerDependenciesArchive objects with the following at the
-    # root level of the protobuf:
-    #
-    #     "baseOwnerUid": {
-    #         "lower": "0x0c4ebfb1d9676393",
-    #         "upper": "0xf9ad9f35d33aba96"
-    #     }
-    #      "formulaOwnerUid": {
-    #         "lower": "0x0c4ebfb1d96763b6",
-    #         "upper": "0xf9ad9f35d33aba96"
-    #     },
-    #
-    # The Table UUID is the TSCE.FormulaOwnerDependenciesArchive whose formulaOwnerUid
-    # matches the UUID of the hauntedOwner of the Table:
-    #
-    #    "hauntedOwner": {
-    #        "ownerUid": {
-    #            "lower": "0x0c4ebfb1d96763b6",
-    #            "upper": "0xf9ad9f35d33aba96"
-    #        }
-    #    }
-    haunted_owner = _uuid(table.haunted_owner.owner_uid)
-    for dependency_id in object_store.find_refs("FormulaOwnerDependenciesArchive"):
-        obj = object_store[dependency_id]
-        if obj.HasField("base_owner_uid") and obj.HasField("formula_owner_uid"):
-            base_owner_uid = _uuid(obj.base_owner_uid)
-            formula_owner_uid = _uuid(obj.formula_owner_uid)
-            if formula_owner_uid == haunted_owner:
-                return base_owner_uid
-
-
-def _extract_cell_records(object_store: ObjectStore, table: Table):
-    """Exract all the formula cell ranges for the Table."""
-    # The TSCE.CalculationEngineArchive contains formulaOwnerInfo records
-    # inside the dependencyTracker. Each of these has a cellDependencies
-    # dictionary and some contain cellRecords that contain formula references:
-    #
-    # "cellDependencies": {
-    #     "cellRecord": [
-    #         {
-    #             "column": 0,
-    #             "containsAFormula": true,
-    #             "edges": { "packedEdgeWithoutOwner": [ 16777216 ] },
-    #             "row": 1
-    #         },
-    #         {
-    #             "column": 1,
-    #             "containsAFormula": true,
-    #             "edges": { "packedEdgeWithoutOwner": [16777216 ] },
-    #             "row": 1
-    #         }
-    #     ]
-    # }
-    cell_records = []
-    table_base_id = _extract_table_base_id(object_store, table)
-    calculation_engine_id = object_store.find_refs("CalculationEngineArchive")[0]
-    formula_owner_info = object_store[calculation_engine_id].dependency_tracker.formula_owner_info
-    for finfo in object_store[calculation_engine_id].dependency_tracker.formula_owner_info:
-        if finfo.HasField("cell_dependencies"):
-            formula_owner_id = _uuid(finfo.formula_owner_id)
-            if formula_owner_id == table_base_id:
-                for cell_record in finfo.cell_dependencies.cell_record:
-                    if cell_record.contains_a_formula:
-                        cell_records.append((cell_record.row, cell_record.column))
-    return cell_records
