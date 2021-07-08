eeb96380ecc6b53dee9b4377fd96f826c55142bf
Jon Connell <python@figsandfudge.com>
2021-07-08 15:20:52 +0100

    Refactored exceptions module

---
+++ src/numbers_parser/exceptions.py
@@ -0,0 +1,28 @@
+class NumbersError(Exception):
+    """Base class for other exceptions"""
+
+    pass
+
+
+class UnsupportedError(NumbersError):
+    """Raised for unsupported file format features"""
+
+    pass
+
+
+class FileError(NumbersError):
+    """Raised for IO and other OS errors"""
+
+    pass
+
+
+class FileFormatError(NumbersError):
+    """Raised for parsing errors during file load"""
+
+    pass
+
+
+class UnsupportedError(NumbersError):
+    """Raised for unsupported file format features"""
+
+    pass
