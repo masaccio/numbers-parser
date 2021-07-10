e2400be06d33afa44a4ceea16f5c49961d594645
Jon Connell <python@figsandfudge.com>
2021-07-10 14:43:10 +0100

    Refactoring to simplify table cell extraction

--- src/numbers_parser/document.py
+++ src/numbers_parser/document.py
@@ -76,15 +76,13 @@ class Table:
 
         bds = self._table.base_data_store
         self._row_headers = [
-            h.numberOfCells for h in object_store[bds.rowHeaders.buckets[0].identifier].headers
+            h.numberOfCells
+            for h in object_store[bds.rowHeaders.buckets[0].identifier].headers
         ]
         self._column_headers = [
             h.numberOfCells for h in object_store[bds.columnHeaders.identifier].headers
         ]
         self._tiles = [object_store[t.tile.identifier] for t in bds.tiles.tiles]
-        self._table_data = None
-        self._table_cells = None
-        self._merge_cells = None
 
     @property
     def name(self):
@@ -103,7 +101,8 @@ class Table:
     def cells(self) -> list:
         """The cells property will be deprecated: use rows instead"""
         warnings.warn(
-            "cells property will be deprecated in 3.0; use rows instead", PendingDeprecationWarning
+            "cells property will be deprecated in 3.0; use rows instead",
+            PendingDeprecationWarning,
         )
         return self.rows(values_only=False)
 
@@ -117,8 +116,6 @@ class Table:
         Returns:
             rows: list of rows; each row is a list of Cell objects
         """
-        if self._table_cells is None:
-            self._table_cells = self._extract_table_cells()
         if values_only:
             row_values = []
             for row_cells in self._table_cells:
@@ -129,61 +126,78 @@ class Table:
 
     @property
     def merge_ranges(self) -> list:
-        if self._merge_cells is None:
+        if not hasattr(self, "_merge_cells"):
             self._merge_cells = get_merge_cell_ranges(self._object_store, self._table)
 
         ranges = [xl_range(*r["rect"]) for r in self._merge_cells.values()]
         return sorted(set(list(ranges)))
 
-    def _extract_table_cells(self):
-        if self._merge_cells is None:
-            self._merge_cells = get_merge_cell_ranges(self._object_store, self._table)
+    def _storage_buffer(self, row_num: int, col_num: int, bnc: bool = False) -> bytes:
+        if not hasattr(self, "_storage_buffers") or not hasattr(
+            self, "_storage_buffers_pre_bnc"
+        ):
+            row_infos = []
+            for tile in self._tiles:
+                row_infos += tile.rowInfos
 
-        row_infos = []
-        for tile in self._tiles:
-            row_infos += tile.rowInfos
+            storage_version = self._tiles[0].storage_version
+            if storage_version != 5:
+                raise UnsupportedError(  # pragma: no cover
+                    f"Unsupported row info version {storage_version}"
+                )
 
-        storage_version = self._tiles[0].storage_version
+            if bnc:
+                self._storage_buffers_pre_bnc = [
+                    get_storage_buffers_for_row(
+                        r.cell_storage_buffer_pre_bnc,
+                        r.cell_offsets_pre_bnc,
+                        self.num_cols,
+                        r.has_wide_offsets,
+                    )
+                    for r in row_infos
+                ]
+            else:
+                self._storage_buffers = [
+                    get_storage_buffers_for_row(
+                        r.cell_storage_buffer,
+                        r.cell_offsets,
+                        self.num_cols,
+                        r.has_wide_offsets,
+                    )
+                    for r in row_infos
+                ]
 
-        if storage_version != 5:
-            raise UnsupportedError(  # pragma: no cover
-                f"Unsupported row info version {storage_version}"
-            )
+        try:
+            if bnc:
+                buffer = self._storage_buffers_pre_bnc[row_num][col_num]
+            else:
+                buffer = self._storage_buffers[row_num][col_num]
+        except IndexError:
+            return None
+        return buffer
 
-        storage_buffers = [
-            _extract_cell_data(
-                r.cell_storage_buffer, r.cell_offsets, self.num_cols, r.has_wide_offsets
-            )
-            for r in row_infos
-        ]
-        storage_buffers_pre_bnc = [
-            _extract_cell_data(
-                r.cell_storage_buffer_pre_bnc,
-                r.cell_offsets_pre_bnc,
-                self.num_cols,
-                r.has_wide_offsets,
-            )
-            for r in row_infos
-        ]
+    @property
+    def _merge_cells(self):
+        if not hasattr(self, "__merge_cells"):
+            self.__merge_cells = get_merge_cell_ranges(self._object_store, self._table)
+        return self.__merge_cells
+
+    @property
+    def _table_cells(self):
+        if hasattr(self, "__table_cells"):
+            return self.__table_cells
 
         table_formulas = TableFormulas(self._object_store, self._table)
 
-        table_cells = []
+        self.__table_cells = []
         num_rows = sum([t.numrows for t in self._tiles])
         for row_num in range(num_rows):
             row = []
             for col_num in range(self.num_cols):
-                if col_num < len(storage_buffers[row_num]):
-                    storage_buffer = storage_buffers[row_num][col_num]
-                else:
-                    # Rest of row is empty cells
-                    row.append(EmptyCell(row_num, col_num))
-                    continue
-
-                if col_num < len(storage_buffers_pre_bnc[row_num]):
-                    storage_buffer_pre_bnc = storage_buffers_pre_bnc[row_num][col_num]
-                else:
-                    storage_buffer_pre_bnc = None
+                storage_buffer = self._storage_buffer(row_num, col_num)
+                storage_buffer_pre_bnc = self._storage_buffer(
+                    row_num, col_num, bnc=True
+                )
 
                 cell = None
                 if storage_buffer is None:
@@ -202,11 +216,10 @@ class Table:
 
                     logging.debug(
                         "%s@[%d,%d]: cell_type=%d, storage_buffer=%s",
-                        self._table.table_name,
+                        self.name,
                         row_num,
                         col_num,
                         cell_type,
-                        # binascii.hexlify does not have sep=':' before 3.7
                         ":".join(["{0:02x}".format(b) for b in storage_buffer]),
                     )
 
@@ -216,35 +229,49 @@ class Table:
                         if storage_buffer_pre_bnc is None:
                             cell_value = 0.0
                         else:
-                            cell_value = struct.unpack("<d", storage_buffer_pre_bnc[-12:-4])[0]
+                            cell_value = struct.unpack(
+                                "<d", storage_buffer_pre_bnc[-12:-4]
+                            )[0]
                         cell = NumberCell(row_num, col_num, cell_value)
                     elif cell_type == TSTArchives.textCellType:
                         string_key = struct.unpack("<i", storage_buffer[12:16])[0]
-                        cell = TextCell(row_num, col_num, self._table_string(string_key))
+                        cell = TextCell(
+                            row_num, col_num, self._table_strings[string_key]
+                        )
                     elif cell_type == TSTArchives.dateCellType:
                         if storage_buffer_pre_bnc is None:
                             cell_value = datetime(2001, 1, 1)
                         else:
-                            seconds = struct.unpack("<d", storage_buffer_pre_bnc[-12:-4])[0]
-                            cell_value = datetime(2001, 1, 1) + timedelta(seconds=seconds)
+                            seconds = struct.unpack(
+                                "<d", storage_buffer_pre_bnc[-12:-4]
+                            )[0]
+                            cell_value = datetime(2001, 1, 1) + timedelta(
+                                seconds=seconds
+                            )
                         cell = DateCell(row_num, col_num, cell_value)
                     elif cell_type == TSTArchives.boolCellType:
                         d = struct.unpack("<d", storage_buffer[12:20])[0]
                         cell = BoolCell(row_num, col_num, d > 0.0)
                     elif cell_type == TSTArchives.durationCellType:
                         cell_value = struct.unpack("<d", storage_buffer[12:20])[0]
-                        cell = DurationCell(row_num, col_num, timedelta(days=cell_value))
+                        cell = DurationCell(
+                            row_num, col_num, timedelta(days=cell_value)
+                        )
                     elif cell_type == TSTArchives.formulaErrorCellType:
                         cell = ErrorCell(row_num, col_num)
                     elif cell_type == TSTArchives.currencyCellValueType:
-                        cell_value = struct.unpack("<d", storage_buffer_pre_bnc[-12:-4])[0]
+                        cell_value = struct.unpack(
+                            "<d", storage_buffer_pre_bnc[-12:-4]
+                        )[0]
                         cell = FormulaCell(row_num, col_num)
                     elif cell_type == 10:
                         # TODO: Is this realy a number cell (experiments suggest so)
                         if storage_buffer_pre_bnc is None:
                             cell_value = 0.0
                         else:
-                            cell_value = struct.unpack("<d", storage_buffer_pre_bnc[-12:-4])[0]
+                            cell_value = struct.unpack(
+                                "<d", storage_buffer_pre_bnc[-12:-4]
+                            )[0]
                         cell = NumberCell(row_num, col_num, cell_value)
                     else:
                         raise UnsupportedError(  # pragma: no cover
@@ -265,27 +292,47 @@ class Table:
                     if table_formulas.is_formula(row_num, col_num):
                         try:
                             if table_formulas.is_error(row_num, col_num):
-                                formula_key = struct.unpack("<h", storage_buffer[12:14])[0]
+                                formula_key = struct.unpack(
+                                    "<h", storage_buffer[12:14]
+                                )[0]
                             if cell_type == TSTArchives.numberCellType:  # 2
-                                formula_key = struct.unpack("<h", storage_buffer[28:30])[0]
+                                formula_key = struct.unpack(
+                                    "<h", storage_buffer[28:30]
+                                )[0]
                             elif cell_type == TSTArchives.textCellType:  # 3
                                 if len(storage_buffer) > 28:
-                                    formula_key = struct.unpack("<h", storage_buffer[20:22])[0]
+                                    formula_key = struct.unpack(
+                                        "<h", storage_buffer[20:22]
+                                    )[0]
                                 else:
-                                    formula_key = struct.unpack("<h", storage_buffer[24:26])[0]
+                                    formula_key = struct.unpack(
+                                        "<h", storage_buffer[24:26]
+                                    )[0]
                             elif cell_type == TSTArchives.dateCellType:  # 5
-                                formula_key = struct.unpack("<h", storage_buffer[24:26])[0]
+                                formula_key = struct.unpack(
+                                    "<h", storage_buffer[24:26]
+                                )[0]
                             elif cell_type == TSTArchives.boolCellType:  # 6
-                                formula_key = struct.unpack("<h", storage_buffer[24:26])[0]
+                                formula_key = struct.unpack(
+                                    "<h", storage_buffer[24:26]
+                                )[0]
                             elif cell_type == TSTArchives.durationCellType:  # 7
-                                formula_key = struct.unpack("<h", storage_buffer[24:26])[0]
+                                formula_key = struct.unpack(
+                                    "<h", storage_buffer[24:26]
+                                )[0]
                             elif cell_type == TSTArchives.formulaErrorCellType:  # 8
-                                formula_key = struct.unpack("<h", storage_buffer[12:14])[0]
+                                formula_key = struct.unpack(
+                                    "<h", storage_buffer[12:14]
+                                )[0]
                             elif cell_type == TSTArchives.currencyCellValueType:  # 9
-                                formula_key = struct.unpack("<h", storage_buffer[32:34])[0]
+                                formula_key = struct.unpack(
+                                    "<h", storage_buffer[32:34]
+                                )[0]
                             elif cell_type == 10:
                                 # Might be [36:38]
-                                formula_key = struct.unpack("<h", storage_buffer[32:34])[0]
+                                formula_key = struct.unpack(
+                                    "<h", storage_buffer[32:34]
+                                )[0]
                             else:
                                 raise UnsupportedError(  # pragma: no cover
                                     f"Unsupported formula cell type '{cell_type}' @{self.name}:({row_num},{col_num})"
@@ -296,7 +343,6 @@ class Table:
                                 row_num,
                                 col_num,
                                 formula_key,
-                                # binascii.hexlify does not have sep=':' before 3.7
                                 ":".join(["{0:02x}".format(b) for b in storage_buffer]),
                             )
 
@@ -314,15 +360,18 @@ class Table:
                             )
 
                     row.append(cell)
-            table_cells.append(row)
+            self.__table_cells.append(row)
 
-        return table_cells
+        return self.__table_cells
 
-    def _table_string(self, key):
-        if not hasattr(self, "_table_strings"):
+    @property
+    def _table_strings(self):
+        if not hasattr(self, "__table_strings"):
             strings_id = self._table.base_data_store.stringTable.identifier
-            self._table_strings = {x.key: x.string for x in self._object_store[strings_id].entries}
-        return self._table_strings[key]
+            self.__table_strings = {
+                x.key: x.string for x in self._object_store[strings_id].entries
+            }
+        return self.__table_strings
 
     @property
     def num_rows(self) -> int:
@@ -391,7 +440,9 @@ class Table:
         rows = self.rows()
         for row_num in range(min_row, max_row + 1):
             if values_only:
-                yield tuple(cell.value or None for cell in rows[row_num][min_col : max_col + 1])
+                yield tuple(
+                    cell.value or None for cell in rows[row_num][min_col : max_col + 1]
+                )
             else:
                 yield tuple(rows[row_num][min_col : max_col + 1])
 
@@ -442,7 +493,7 @@ class Table:
                 yield tuple(row[col_num] for row in rows[min_row : max_row + 1])
 
 
-def _extract_cell_data(
+def get_storage_buffers_for_row(
     storage_buffer: bytes, offsets: list, num_cols: int, has_wide_offsets: bool
 ) -> List[bytes]:
     """
@@ -476,7 +527,9 @@ def _extract_cell_data(
         else:
             # Get next offset past current one that is not -1
             # https://stackoverflow.com/questions/19502378/
-            idx = next((i for i, x in enumerate(offsets[col_num + 1 :]) if x >= 0), None)
+            idx = next(
+                (i for i, x in enumerate(offsets[col_num + 1 :]) if x >= 0), None
+            )
             if idx is None:
                 end = len(storage_buffer)
             else:
