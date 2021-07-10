cbec942ae921c8b6598749a0643337611c8c1491
Jon Connell <python@figsandfudge.com>
2021-07-10 14:33:50 +0100

    Config files for VS Code

--- src/encode_double.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python3
-
-from binascii import hexlify
-from struct import pack
-from sys import argv
-
-for arg in argv[1:]:
-  print(arg, "=", hexlify(pack("<d", float(arg)), sep=":"))
