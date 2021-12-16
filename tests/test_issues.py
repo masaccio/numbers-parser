6baa55ae6d43e1e3a84d4f1a11cb29f3118ae404
Jon Connell <python@figsandfudge.com>
2021-12-16 09:24:42 +0000

    Test multi-line strings and unicode

--- tests/test_issues.py
+++ tests/test_issues.py
@@ -3,6 +3,10 @@ import pytest
 from numbers_parser import Document, NumberCell
 
 ISSUE_3_REF = [("A", "B"), (2.0, 0.0), (3.0, 1.0), (None, None)]
+ISSUE_4_REF_1 = "Part 1 \n\nPart 2\n"
+ISSUE_4_REF_2 = "\n\nPart 1 \n\n\nPart 2\n\n"
+ISSUE_4_REF_3 = "今天是个好日子"
+ISSUE_4_REF_4 = "Lorem ipsum\n\ndolor sit amet,\n\nconsectetur adipiscing"
 
 
 def test_issue_3():
@@ -19,3 +23,14 @@ def test_issue_3():
     for row in tables[0].iter_rows(values_only=True):
         ref.append(row)
     assert ref == ISSUE_3_REF
+
+
+def test_issue_4():
+    doc = Document("tests/data/issue-4.numbers")
+    sheets = doc.sheets()
+    tables = sheets[0].tables()
+    table = tables[0]
+    assert table.cell(1, 0).value == ISSUE_4_REF_1
+    assert table.cell(1, 1).value == ISSUE_4_REF_2
+    assert table.cell(2, 0).value == ISSUE_4_REF_3
+    assert table.cell(2, 1).value == ISSUE_4_REF_4
