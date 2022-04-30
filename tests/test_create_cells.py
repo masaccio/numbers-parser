3dc972d7e363c5a8a63821a88ae9b089796d0278
Jon Connell <python@figsandfudge.com>
2022-04-30 18:38:43 +0100

    Test main cell types

--- tests/test_create_cells.py
+++ tests/test_create_cells.py
@@ -1,5 +1,7 @@
 import pytest
 import os
+
+from datetime import datetime, timedelta
 from tempfile import TemporaryDirectory
 
 from numbers_parser import Document
@@ -13,14 +15,20 @@ def test_edit_strings():
     tables = sheets[0].tables()
     table = tables[0]
     table.write("B2", "new_b2")
-    table.write(2, 1, "new_c2")
-    table.write(5, 4, "new_f7")
+    table.write(1, 2, "new_c2")
+    table.write(2, 0, True)
+    table.write(2, 1, 1234)
+    table.write(2, 2, 12.34)
+    table.write(5, 3, datetime(2020, 12, 25))
+    table.write(5, 4, timedelta(seconds=1234))
+    table.write(5, 5, "1234")
 
     assert isinstance(table.cell(3, 4), EmptyCell)
     assert isinstance(table.cell(4, 4), EmptyCell)
 
     temp_dir = TemporaryDirectory()
-    new_filename = os.path.join(temp_dir.name, "test-save-1-new.numbers")
+    # new_filename = os.path.join(temp_dir.name, "test-save-1-new.numbers")
+    new_filename = "/Users/jon/Downloads/test-save-1-new.numbers"
     doc.save(new_filename)
 
     doc = Document(new_filename)
@@ -29,5 +37,10 @@ def test_edit_strings():
     table = tables[0]
 
     assert table.cell(1, 1).value == "new_b2"
-    assert table.cell(2, 1).value == "new_c2"
-    assert table.cell(5, 4).value == "new_f7"
+    assert table.cell("C2").value == "new_c2"
+    assert table.cell(2, 0).value == True
+    assert table.cell(2, 1).value == 1234
+    assert table.cell(2, 2).value == 12.34
+    assert table.cell(5, 3).value == datetime(2020, 12, 25)
+    assert table.cell(5, 4).value == timedelta(seconds=1234)
+    assert table.cell(5, 5).value == "1234"
