d055427d1df2073e55944975b387a933fa60ffb4
Jon Connell <python@figsandfudge.com>
2022-05-16 20:27:40 +0100

    Negative index

--- src/numbers_parser/containers.py
+++ src/numbers_parser/containers.py
@@ -9,7 +9,9 @@ class ItemsList:
 
     def __getitem__(self, key: int):
         if type(key) == int:
-            if key < 0 or key >= len(self._items):
+            if key < 0:
+                key += len(self._items)
+            if key >= len(self._items):
                 raise IndexError(f"index {key} out of range")
             return self._items[key]
         elif type(key) == str:
