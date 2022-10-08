d6bb8bc31c0fbd1a7a094c8c93ef17ec66e25280
Jon Connell <python@figsandfudge.com>
2022-10-08 16:34:51 +0100

    Format stress test generator

--- src/debug/generate_custom_formats.py
+++ src/debug/generate_custom_formats.py
@@ -1,3 +1,5 @@
+import math
+
 from enum import Enum
 from uuid import uuid1
 
@@ -58,7 +60,8 @@ def next_format_key(table):
     for entry in format_table.entries:
         if entry.key > last_key:
             last_key = entry.key
-    return last_key + 1
+    format_table.nextListID = last_key + 1
+    return format_table.nextListID
 
 
 def custom_format(
@@ -75,17 +78,21 @@ def custom_format(
     format_name += "Dec" + str(num_decimals) + "_"
     format_name += "Sep" if show_thousands_separator else "NoSep"
 
-    format_string = integer_format.value[1] * num_integers
+    if integer_format.value[1] == "-" or num_integers == 0:
+        format_string = ""
+    else:
+        format_string = integer_format.value[1] * num_integers
     if num_decimals > 0:
-        format_string += "." + decimal_format.value[1] * num_decimals
+        if decimal_format.value[1] == "-":
+            format_string += "." + "0" * num_decimals
+        else:
+            format_string += "." + decimal_format.value[1] * num_decimals
 
     min_integer_width = 0
 
     num_nonspace_decimal_digits = 0
 
     num_nonspace_integer_digits = 0
-
-    total_num_decimal_digits = 0
 
     custom_format = TSKArchives.CustomFormatArchive(
         name=format_name,
@@ -97,7 +104,7 @@ def custom_format(
             decimal_width=num_decimals,
             format_type=270,
             fraction_accuracy=0xFFFFFFFF,
-            is_complex=False,
+            # is_complex=False,
             min_integer_width=min_integer_width,
             num_hash_decimal_digits=0,
             num_nonspace_decimal_digits=num_nonspace_decimal_digits,
@@ -105,7 +112,7 @@ def custom_format(
             requires_fraction_replacement=False,
             scale_factor=1.0,
             show_thousands_separator=show_thousands_separator,
-            total_num_decimal_digits=total_num_decimal_digits,
+            total_num_decimal_digits=num_decimals,
             use_accounting_style=False,
         ),
     )
@@ -116,16 +123,60 @@ def custom_format(
     return (uuid, custom_format)
 
 
+def text_reference(
+    value,
+    integer_format=FormatType.INT_DEFAULT,
+    decimal_format=FormatType.DEC_DEFAULT,
+    num_integers=0,
+    num_decimals=0,
+    show_thousands_separator=False,
+):
+    integer = int(value) if num_decimals > 0 else round(value)
+    decimal = math.modf(value)[0]
+    if integer_format.value[1] == "0":
+        if show_thousands_separator and integer != 0:
+            num_integers += int(math.floor(math.log10(integer)) / 3)
+            formatted_value = f"{integer:0{num_integers},}"
+        else:
+            formatted_value = f"{integer:0{num_integers}}"
+    else:
+        if show_thousands_separator and integer != 0:
+            num_integers += int(math.floor(math.log10(integer)) / 3)
+            formatted_value = f"{integer:{num_integers},}"
+        else:
+            formatted_value = str(integer)
+
+    if num_decimals > 0:
+        decimal = round(decimal, num_decimals)
+        if decimal_format.value[1] == "0":
+            formatted_value += "." + f"{decimal:.{num_decimals}f}"[2:]
+        elif decimal_format.value[1] == "-":
+            formatted_value += str(decimal).ljust(num_decimals)
+        else:
+            formatted_value += "." + str(decimal)[2:]
+    return formatted_value
+
+
 def write_formula_key(table, row_num, col_num, id):
     table.cell(row_num, col_num)._storage.formula_id = id
 
 
 def write_format_key(table, row_num, col_num, id):
+    table.cell(row_num, col_num)._storage.suggest_id = 1
     table.cell(row_num, col_num)._storage.num_format_id = id
 
 
-doc = Document("tests/data/custom-format-stress.numbers")
+doc = Document("tests/data/custom-format-stress-template.numbers")
 table = doc.sheets[0].tables[0]
+table.col_width(0, 200)
+table.col_width(1, 30)
+table.col_width(2, 30)
+table.col_width(3, 35)
+table.col_width(4, 35)
+table.col_width(5, 40)
+table.col_width(6, 50)
+table.col_width(7, 100)
+table.col_width(8, 130)
 
 custom_format_list_id = doc._model.objects[
     DOCUMENT_ID
@@ -134,55 +185,66 @@ custom_format_list = doc._model.objects[custom_format_list_id]
 
 test_formula_key = table.cell(1, 2)._storage.formula_id
 test_format_id = table.cell(1, 2)._storage.num_format_id
-ref_formula_key = table.cell(1, 3)._storage.formula_id
 
 row_num = 2
-try:
-    for integer_format in [
-        FormatType.INT_DEFAULT,
-        FormatType.INT_ZEROS,
-        FormatType.INT_SPACES,
+for integer_format in [
+    FormatType.INT_DEFAULT,
+    FormatType.INT_ZEROS,
+    FormatType.INT_SPACES,
+]:
+    for decimal_format in [
+        FormatType.DEC_DEFAULT,
+        FormatType.DEC_ZEROS,
+        FormatType.DEC_SPACES,
     ]:
-        for decimal_format in [
-            FormatType.DEC_DEFAULT,
-            FormatType.DEC_ZEROS,
-            FormatType.DEC_SPACES,
-        ]:
-            for num_integers in range(0, 10):
-                for num_decimals in range(0, 10):
-                    for show_thousands_separator in [False, True]:
-                        (uuid, format) = custom_format(
-                            integer_format=FormatType.INT_DEFAULT,
-                            decimal_format=FormatType.DEC_DEFAULT,
-                            num_integers=num_integers,
-                            num_decimals=num_decimals,
-                            show_thousands_separator=show_thousands_separator,
-                        )
-                        custom_format_list.custom_formats.append(format)
-                        custom_format_list.uuids.append(uuid)
-                        format_id = next_format_key(table)
-                        add_format_to_table_data(
-                            table, custom_format_list.uuids[-1], format_id
-                        )
+        for num_integers in range(0, 10):
+            for num_decimals in range(0, 10):
+                if num_integers == 0 and num_decimals == 0:
+                    continue
+                for show_thousands_separator in [False, True]:
+                    (uuid, format) = custom_format(
+                        integer_format=integer_format,
+                        decimal_format=decimal_format,
+                        num_integers=num_integers,
+                        num_decimals=num_decimals,
+                        show_thousands_separator=show_thousands_separator,
+                    )
+                    custom_format_list.custom_formats.append(format)
+                    custom_format_list.uuids.append(uuid)
+                    format_id = next_format_key(table)
+                    add_format_to_table_data(
+                        table, custom_format_list.uuids[-1], format_id
+                    )
 
-                        for value in [
-                            0.23,
-                            2.34,
-                            23.45,
-                            234.56,
-                            2345.67,
-                        ]:
-                            table.write(row_num, 0, format.name)
-                            table.write(row_num, 1, value)
-                            table.write(row_num, 2, value)
-                            table.write(row_num, 3, "")
-                            write_format_key(table, row_num, 2, format_id)
-                            write_formula_key(table, row_num, 2, test_formula_key)
-                            write_formula_key(table, row_num, 3, ref_formula_key)
-                            row_num += 1
-                            if row_num > 3:
-                                raise
-except:
-    print(f"Stopping after {row_num} rows")
+                    for value in [
+                        0.23,
+                        2.34,
+                        23.45,
+                        234.56,
+                        2345.67,
+                    ]:
+                        table.write(row_num, 0, format.name)
+                        table.write(row_num, 1, integer_format.value)
+                        table.write(row_num, 2, decimal_format.value)
+                        table.write(row_num, 3, num_integers)
+                        table.write(row_num, 4, num_decimals)
+                        table.write(row_num, 5, show_thousands_separator)
+                        table.write(row_num, 6, value)
+                        table.write(row_num, 7, value)
+                        write_format_key(table, row_num, 7, format_id)
+                        write_formula_key(table, row_num, 7, test_formula_key)
+                        table.write(
+                            row_num,
+                            8,
+                            text_reference(
+                                value,
+                                integer_format=integer_format,
+                                decimal_format=decimal_format,
+                                num_integers=num_integers,
+                                num_decimals=num_decimals,
+                                show_thousands_separator=show_thousands_separator,
+                            ),
+                        )
+                        row_num += 1
 
-doc.save("custom-format-stress.numbers")
+doc.save("tests/data/custom-format-stress.numbers")
