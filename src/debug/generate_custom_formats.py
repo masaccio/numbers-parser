b4493a193bb9923adb1efc16175ead7516560ab1
Jon Connell <python@figsandfudge.com>
2022-10-10 20:21:26 +0100

    Remove text reference

--- src/debug/generate_custom_formats.py
+++ src/debug/generate_custom_formats.py
@@ -1,4 +1,5 @@
 import math
+import re
 
 from enum import Enum
 from uuid import uuid1
@@ -10,13 +11,13 @@ from numbers_parser.generated import TSTArchives_pb2 as TSTArchives
 from numbers_parser.generated import TSPMessages_pb2 as TSPMessages
 
 
-class FormatType(Enum):
-    INT_DEFAULT = "I#"
-    INT_ZEROS = "I0"
-    INT_SPACES = "I-"
-    DEC_DEFAULT = "D#"
-    DEC_ZEROS = "D0"
-    DEC_SPACES = "D-"
+class PaddingType(Enum):
+    NONE = 0
+    ZEROS = 1
+    SPACES = 2
+
+    def __str__(self):
+        return str(self.name).capitalize()
 
 
 def add_format_to_table_data(table, format_uuid, format_id):
@@ -52,6 +53,44 @@ def add_format_to_table_data(table, format_uuid, format_id):
     )
 
 
+# contains_integer_token:
+#     true if number of integer spaces > 0
+# custom_format_string:
+#     # for default, 0 for format as spaces/zeros (does not match UI)
+# decimal_width:
+#     number of decimals if decimal padding is spaces
+# format_type:
+#     always 270
+# fraction_accuracy:
+#     always 0xfffffffd
+# index_from_right_last_integer:
+#     number of decimal places + 1 if the number of integer places > 1
+#     number of decimal places + 1 if the number of integer places is zero
+# is_complex
+#     "0" in format string (non-zero numbers of space/zero padding) and either
+#     min_integer_width is positive or num_nonspace_decimal_digits is zero
+# min_integer_width:
+#     number of integer spaces when integer padding is zeroes or spaces
+# num_hash_decimal_digits:
+#     always zero
+# num_nonspace_decimal_digits:
+#     number of decimals if decimal padding is zeros and number of
+#     decimal spaces > 0
+# num_nonspace_integer_digits:
+#     number of integers if integer padding is zeros and number of
+#     integer spaces > 0
+# requires_fraction_replacement:
+#     always false for custom number formats
+# scale_factor:
+#     always 1.0 (for regular custom number formats without a scale)
+# show_thousands_separator:
+#     true if integers have commas and number of integer spaces > 0
+# total_num_decimal_digits:
+#     number of decimals if decimal padding is spaces
+# use_accounting_style:
+#     always false for custom formats
+
+
 def next_format_key(table):
     base_data_store = table._model.objects[table._table_id].base_data_store
     format_table_id = base_data_store.format_table.identifier
@@ -65,54 +104,77 @@ def next_format_key(table):
 
 
 def custom_format(
-    integer_format=FormatType.INT_DEFAULT,
-    decimal_format=FormatType.DEC_DEFAULT,
+    integer_format=PaddingType.NONE,
+    decimal_format=PaddingType.NONE,
     num_integers=0,
     num_decimals=0,
     show_thousands_separator=False,
 ) -> object:
     format_name = "Format_"
-    format_name += integer_format.value + "_"
+    format_name += "I=" + str(integer_format) + "_"
     format_name += "Int" + str(num_integers) + "_"
-    format_name += decimal_format.value + "_"
+    format_name += "D=" + str(decimal_format) + "_"
     format_name += "Dec" + str(num_decimals) + "_"
     format_name += "Sep" if show_thousands_separator else "NoSep"
 
-    if integer_format.value[1] == "-" or num_integers == 0:
+    if num_integers == 0:
         format_string = ""
+    elif integer_format == PaddingType.NONE:
+        format_string = "#" * num_integers
     else:
-        format_string = integer_format.value[1] * num_integers
+        format_string = "0" * num_integers
+    if num_integers > 6:
+        format_string = re.sub(r"(...)(...)$", r",\1,\2", format_string)
+    elif num_integers > 3:
+        format_string = re.sub(r"(...)$", r",\1", format_string)
     if num_decimals > 0:
-        if decimal_format.value[1] == "-":
-            format_string += "." + "0" * num_decimals
+        if decimal_format == PaddingType.NONE:
+            format_string += "." + "#" * num_decimals
         else:
-            format_string += "." + decimal_format.value[1] * num_decimals
+            format_string += "." + "0" * num_decimals
 
-    min_integer_width = 0
-
-    num_nonspace_decimal_digits = 0
-
-    num_nonspace_integer_digits = 0
+    min_integer_width = (
+        num_integers if num_integers > 0 and integer_format != PaddingType.NONE else 0
+    )
+    num_nonspace_decimal_digits = (
+        num_decimals if decimal_format == PaddingType.ZEROS else 0
+    )
+    num_nonspace_integer_digits = (
+        num_integers if integer_format == PaddingType.ZEROS else 0
+    )
+    index_from_right_last_integer = (
+        num_decimals + 1 if num_integers > 0 else num_decimals
+    )
+    decimal_width = (
+        num_decimals
+        if decimal_format == PaddingType.SPACES and integer_format != PaddingType.ZEROS
+        else 0
+    )
+    # TODO: simplify
+    is_complex = "0" in format_string and (
+        min_integer_width > 0 or num_nonspace_decimal_digits == 0
+    )
 
     custom_format = TSKArchives.CustomFormatArchive(
         name=format_name,
         format_type_pre_bnc=270,
         format_type=270,
         default_format=TSKArchives.FormatStructArchive(
-            contains_integer_token=False,
+            contains_integer_token=num_integers > 0,
             custom_format_string=format_string,
-            decimal_width=num_decimals,
+            decimal_width=decimal_width,
             format_type=270,
-            fraction_accuracy=0xFFFFFFFF,
-            # is_complex=False,
+            fraction_accuracy=0xFFFFFFFD,
+            index_from_right_last_integer=index_from_right_last_integer,
+            is_complex=is_complex,
             min_integer_width=min_integer_width,
             num_hash_decimal_digits=0,
             num_nonspace_decimal_digits=num_nonspace_decimal_digits,
             num_nonspace_integer_digits=num_nonspace_integer_digits,
             requires_fraction_replacement=False,
             scale_factor=1.0,
-            show_thousands_separator=show_thousands_separator,
-            total_num_decimal_digits=num_decimals,
+            show_thousands_separator=show_thousands_separator and num_integers > 0,
+            total_num_decimal_digits=decimal_width,
             use_accounting_style=False,
         ),
     )
@@ -121,40 +183,6 @@ def custom_format(
         lower=int(hex_uuid[0:16], 16), upper=int(hex_uuid[16:32], 16)
     )
     return (uuid, custom_format)
-
-
-def text_reference(
-    value,
-    integer_format=FormatType.INT_DEFAULT,
-    decimal_format=FormatType.DEC_DEFAULT,
-    num_integers=0,
-    num_decimals=0,
-    show_thousands_separator=False,
-):
-    integer = int(value) if num_decimals > 0 else round(value)
-    decimal = math.modf(value)[0]
-    if integer_format.value[1] == "0":
-        if show_thousands_separator and integer != 0:
-            num_integers += int(math.floor(math.log10(integer)) / 3)
-            formatted_value = f"{integer:0{num_integers},}"
-        else:
-            formatted_value = f"{integer:0{num_integers}}"
-    else:
-        if show_thousands_separator and integer != 0:
-            num_integers += int(math.floor(math.log10(integer)) / 3)
-            formatted_value = f"{integer:{num_integers},}"
-        else:
-            formatted_value = str(integer)
-
-    if num_decimals > 0:
-        decimal = round(decimal, num_decimals)
-        if decimal_format.value[1] == "0":
-            formatted_value += "." + f"{decimal:.{num_decimals}f}"[2:]
-        elif decimal_format.value[1] == "-":
-            formatted_value += str(decimal).ljust(num_decimals)
-        else:
-            formatted_value += "." + str(decimal)[2:]
-    return formatted_value
 
 
 def write_formula_key(table, row_num, col_num, id):
@@ -168,9 +196,9 @@ def write_format_key(table, row_num, col_num, id):
 
 doc = Document("tests/data/custom-format-stress-template.numbers")
 table = doc.sheets[0].tables[0]
-table.col_width(0, 200)
-table.col_width(1, 30)
-table.col_width(2, 30)
+table.col_width(0, 250)
+table.col_width(1, 50)
+table.col_width(2, 50)
 table.col_width(3, 35)
 table.col_width(4, 35)
 table.col_width(5, 40)
@@ -188,15 +216,17 @@ test_format_id = table.cell(1, 2)._storage.num_format_id
 
 row_num = 2
 for integer_format in [
-    FormatType.INT_DEFAULT,
-    FormatType.INT_ZEROS,
-    FormatType.INT_SPACES,
+    PaddingType.NONE,
+    PaddingType.ZEROS,
+    PaddingType.SPACES,
 ]:
     for decimal_format in [
-        FormatType.DEC_DEFAULT,
-        FormatType.DEC_ZEROS,
-        FormatType.DEC_SPACES,
+        PaddingType.NONE,
+        PaddingType.ZEROS,
+        PaddingType.SPACES,
     ]:
+        # for num_integers in [0, 5]:
+        #     for num_decimals in [0, 5]:
         for num_integers in range(0, 10):
             for num_decimals in range(0, 10):
                 if num_integers == 0 and num_decimals == 0:
@@ -224,8 +254,8 @@ for integer_format in [
                         2345.67,
                     ]:
                         table.write(row_num, 0, format.name)
-                        table.write(row_num, 1, integer_format.value)
-                        table.write(row_num, 2, decimal_format.value)
+                        table.write(row_num, 1, str(integer_format))
+                        table.write(row_num, 2, str(decimal_format))
                         table.write(row_num, 3, num_integers)
                         table.write(row_num, 4, num_decimals)
                         table.write(row_num, 5, show_thousands_separator)
@@ -233,18 +263,8 @@ for integer_format in [
                         table.write(row_num, 7, value)
                         write_format_key(table, row_num, 7, format_id)
                         write_formula_key(table, row_num, 7, test_formula_key)
-                        table.write(
-                            row_num,
-                            8,
-                            text_reference(
-                                value,
-                                integer_format=integer_format,
-                                decimal_format=decimal_format,
-                                num_integers=num_integers,
-                                num_decimals=num_decimals,
-                                show_thousands_separator=show_thousands_separator,
-                            ),
-                        )
+                        table.write(row_num, 8, "0.0")
                         row_num += 1
 
 doc.save("tests/data/custom-format-stress.numbers")
+print("Saved to tests/data/custom-format-stress.numbers")
