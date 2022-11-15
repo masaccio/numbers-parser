74d21ce10f5dcf8bf986511391c7c7ec688fc7aa
Jon Connell <python@figsandfudge.com>
2022-11-15 13:49:12 +0000

    New enums

--- src/numbers_parser/cell_storage.py
+++ src/numbers_parser/cell_storage.py
@@ -14,6 +14,11 @@ from numbers_parser.numbers_uuid import NumbersUUID
 from numbers_parser.generated import TSTArchives_pb2 as TSTArchives
 
 
+SECONDS_IN_HOUR = 60 * 60
+SECONDS_IN_DAY = SECONDS_IN_HOUR * 24
+SECONDS_IN_WEEK = SECONDS_IN_DAY * 7
+
+
 class CellType(Enum):
     EMPTY = 1
     NUMBER = 2
@@ -25,29 +30,39 @@ class CellType(Enum):
     BULLET = 8
 
 
-SECONDS_IN_HOUR = 60 * 60
-SECONDS_IN_DAY = SECONDS_IN_HOUR * 24
-SECONDS_IN_WEEK = SECONDS_IN_DAY * 7
-
-FORMAT_STYLE_NONE = 0
-FORMAT_STYLE_SHORT = 1
-FORMAT_STYLE_MEDIUM = 2
+class CellPadding(Enum):
+    SPACE = 1
+    ZERO = 2
 
 
-class CustomFormatType(Enum):
-    X1 = 1
-    X2 = 256
-    X3 = 257
-    X4 = 258
-    X5 = 260
-    X6 = 261
-    X7 = 262
-    X8 = 268
+class FormatStyle(Enum):
+    NONE = 0
+    SHORT = 1
+    MEDIUM = 2
+
+    def __eq__(self, other):
+        if type(other) == int:
+            return self.value == other
+        else:
+            return self.value == other.value
+
+
+class FormatType(Enum):
+    BOOLEAN = 1
+    DECIMAL = 256
+    CURRENCY = 257
+    PERCENT = 258
+    TEXT = 260
+    DATE = 261
+    FRACTION = 262
+    CHECKBOX = 263
+    RATING = 267
+    DURATION = 268
     X9 = 269
-    NUMBER = 270
-    TEXT = 271
-    DATE = 272
-    CURRENCY = 274
+    CUSTOM_NUMBER = 270
+    CUSTOM_TEXT = 271
+    CUSTOM_DATE = 272
+    CUSTOM_CURRENCY = 274
 
     def __eq__(self, other):
         if type(other) == int:
@@ -250,7 +265,7 @@ class CellStorage:
                     formatted_value = float_to_n_digit_fraction(self.d128, num_digits)
                 else:
                     formatted_value = float_to_fraction(self.d128, accuracy)
-            elif custom_format.format_type == CustomFormatType.TEXT:
+            elif custom_format.format_type == FormatType.CUSTOM_TEXT:
                 if self.string_id is not None:
                     formatted_value = decode_text_format(
                         custom_format,
@@ -259,8 +274,8 @@ class CellStorage:
                 else:
                     return ""
             elif (
-                custom_format.format_type == CustomFormatType.NUMBER
-                or custom_format.format_type == CustomFormatType.CURRENCY
+                custom_format.format_type == FormatType.CUSTOM_NUMBER
+                or custom_format.format_type == FormatType.CUSTOM_CURRENCY
             ):
                 formatted_value = decode_number_format(
                     custom_format, self.d128, format_map[format_uuid].name
@@ -280,7 +295,7 @@ class CellStorage:
             format_map = self.model.custom_format_map()
             custom_format = format_map[format_uuid].default_format
             custom_format_string = custom_format.custom_format_string
-            if custom_format.format_type == CustomFormatType.DATE:
+            if custom_format.format_type == FormatType.CUSTOM_DATE:
                 formatted_value = decode_date_format(
                     custom_format_string, self.datetime
                 )
@@ -324,7 +339,7 @@ class CellStorage:
             dd = int(d / 60)
             if unit_smallest > 8:
                 d -= 60 * dd
-            if duration_style == FORMAT_STYLE_NONE:
+            if duration_style == FormatStyle.NONE:
                 pad = (unit_largest == 8 and unit_smallest == 8) or dd > 10
                 dstr.append(("" if pad else "0") + str(dd))
             else:
@@ -333,14 +348,14 @@ class CellStorage:
             dd = int(d)
             if unit_smallest > 16:
                 d -= dd
-            if duration_style == FORMAT_STYLE_NONE:
+            if duration_style == FormatStyle.NONE:
                 pad = (unit_smallest == 16 and unit_largest == 16) or dd >= 10
                 dstr.append(("" if pad else "0") + str(dd))
             else:
                 dstr.append(str(dd) + unit_format("second", dd, duration_style))
         if unit_smallest >= 32:
             dd = int(round(1000 * d))
-            if duration_style == FORMAT_STYLE_NONE:
+            if duration_style == FormatStyle.NONE:
                 padding = "0" if dd >= 10 else "00"
                 padding = "" if dd >= 100 else padding
                 dstr.append(f"{padding}{dd}")
@@ -349,7 +364,7 @@ class CellStorage:
                     str(dd) + unit_format("millisecond", dd, duration_style, "ms")
                 )
         duration_str = (":" if duration_style == 0 else " ").join(dstr)
-        if duration_style == FORMAT_STYLE_NONE:
+        if duration_style == FormatStyle.NONE:
             duration_str = re.sub(r":(\d\d\d)$", r".\1", duration_str)
         return duration_str
 
@@ -503,17 +518,13 @@ def decode_number_format(format, value, name):  # noqa: C901
     num_decimals = len(dec_part)
     if num_decimals > 0:
         if dec_part[0] == "#":
-            dec_pad_zero = False
-            dec_pad_space = False
+            dec_pad = None
         elif format.num_nonspace_decimal_digits > 0:
-            dec_pad_zero = True
-            dec_pad_space = False
+            dec_pad = CellPadding.ZERO
         else:
-            dec_pad_zero = False
-            dec_pad_space = True
+            dec_pad = CellPadding.SPACE
     else:
-        dec_pad_zero = False
-        dec_pad_space = False
+        dec_pad = None
     dec_width = num_decimals
 
     (integer, decimal) = str(float(value)).split(".")
@@ -527,12 +538,10 @@ def decode_number_format(format, value, name):  # noqa: C901
     num_integers = len(int_part.replace(",", ""))
     if num_integers > 0:
         if int_part[0] == "#":
-            int_pad_zero = False
-            int_pad_space = False
+            int_pad = None
             int_width = len(int_part)
         elif format.num_nonspace_integer_digits > 0:
-            int_pad_zero = True
-            int_pad_space = False
+            int_pad = CellPadding.ZERO
             if format.show_thousands_separator:
                 if integer != 0:
                     num_commas = int(math.floor(math.log10(integer)) / 3)
@@ -543,27 +552,39 @@ def decode_number_format(format, value, name):  # noqa: C901
             else:
                 int_width = num_integers
         else:
-            int_pad_zero = False
-            int_pad_space = True
+            int_pad = CellPadding.SPACE
             int_width = len(int_part)
     else:
-        int_pad_zero = False
-        int_pad_space = False
+        int_pad = None
         int_width = num_integers
 
-    if integer == 0 and int_pad_space and (dec_pad_space or dec_pad_zero):
+    # Formatting integer zero:
+    #   Blank (padded if needed) if int_pad is SPACE and no decimals
+    #   No leading zero if:
+    #     int_pad is NONE, dec_pad is SPACE
+    #     int_pad is SPACE, dec_pad is SPACE
+    #     int_pad is SPACE, dec_pad is ZERO
+    #     int_pad is SPACE, dec_pad is NONE if num decimals < decimals length
+    if integer == 0 and int_pad == CellPadding.SPACE and num_decimals == 0:
         formatted_value = "".rjust(int_width)
-    elif integer == 0 and (not int_pad_zero and not int_pad_space and dec_pad_space):
+    elif integer == 0 and int_pad is None and dec_pad == CellPadding.SPACE:
         formatted_value = ""
-    elif int_pad_zero:
+    elif integer == 0 and int_pad == CellPadding.SPACE and dec_pad is not None:
+        formatted_value = ""
+    elif (
+        integer == 0
+        and int_pad == CellPadding.SPACE
+        and dec_pad is None
+        and len(str(decimal)) > num_decimals
+    ):
+        formatted_value = "".rjust(int_width)
+    elif int_pad == CellPadding.ZERO:
         if format.show_thousands_separator:
             formatted_value = f"{integer:0{int_width},}"
         else:
             formatted_value = f"{integer:0{int_width}}"
-    elif int_pad_space:
-        if integer == 0:
-            formatted_value = "".rjust(int_width)
-        elif format.show_thousands_separator:
+    elif int_pad == CellPadding.SPACE:
+        if format.show_thousands_separator:
             formatted_value = f"{integer:,}".rjust(int_width)
         else:
             formatted_value = str(integer).rjust(int_width)
@@ -576,11 +597,13 @@ def decode_number_format(format, value, name):  # noqa: C901
     if num_decimals:
         # Possible Numbers bug: decimal padding with spaces is rendered as
         # zeroes when there is no integer format
-        if dec_pad_zero or (dec_pad_space and num_integers == 0):
+        if dec_pad == CellPadding.ZERO or (
+            dec_pad == CellPadding.SPACE and num_integers == 0
+        ):
             formatted_value += "." + f"{decimal:,.{dec_width}f}"[2:]
-        elif dec_pad_space and decimal == 0:
+        elif dec_pad == CellPadding.SPACE and decimal == 0:
             formatted_value += ".".ljust(dec_width + 1)
-        elif dec_pad_space:
+        elif dec_pad == CellPadding.SPACE:
             decimal_str = str(decimal)[2:]
             formatted_value += "." + decimal_str.ljust(dec_width)
         elif decimal or num_integers == 0:
@@ -625,12 +648,12 @@ def unit_format(unit: str, value: int, style: int, abbrev: str = None):
     plural = "" if value == 1 else "s"
     if abbrev is None:
         abbrev = unit[0]
-    if style >= FORMAT_STYLE_MEDIUM:
-        return f" {unit}" + plural
-    elif style == FORMAT_STYLE_SHORT:
+    if style == FormatStyle.NONE:
+        return ""
+    elif style == FormatStyle.SHORT:
         return f"{abbrev}"
     else:
-        return ""
+        return f" {unit}" + plural
 
 
 def auto_units(cell_value, format):
