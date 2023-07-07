9efb33cc768fc836fa6ca275ee4cd0b8da95343a
Jon Connell <python@figsandfudge.com>
2023-07-07 09:31:11 +0100

    Test all fonts

--- tests/test_styles.py
+++ tests/test_styles.py
@@ -1,4 +1,4 @@
-from numbers_parser import Document
+from numbers_parser import Document, EmptyCell
 
 TEST_NUMBERED_REF = [
     "(1) double-paren-1",
@@ -48,21 +48,57 @@ def test_bg_colors():
     assert tables["Default Colours"].cell(1, 5).bg_color == (255, 66, 161)
 
 
-def test_fonts():
-    doc = Document("tests/data/test-fonts.numbers")
+def test_styles():
+    doc = Document("tests/data/test-styles.numbers")
     sheets = doc.sheets
     table = sheets[0].tables[0]
 
-    assert table.cell("A3").style_name == "Heading Red"
-    assert table.cell("B2").style_name == "ArialBold14"
-    assert table.cell("C2").style_name == "CourierNew12"
-    assert table.cell("B2").is_bold
-    assert not table.cell("B3").is_bold
-    assert table.cell("C2").is_italic
-    assert table.cell("D2").font_color == (29, 177, 0)
-    assert table.cell("B2").font_size == 14.0
-    assert table.cell("C2").font_size == 12.0
-    assert table.cell("D2").font_size == 11.0
-    assert table.cell("B2").font_name == "Arial-Black"
-    assert table.cell("C2").font_name == "CourierNewPSMT"
-    assert table.cell("D2").font_name == "HelveticaNeue"
+    for row_num, row in enumerate(table.iter_rows()):
+        if row_num == 0:
+            for cell in row[1:]:
+                assert cell.style_name == cell.value
+        elif row_num == 1:
+            assert row[1].style_name == "CustomStyle1"
+            assert row[1].is_bold
+            assert row[1].is_italic
+            assert not row[1].is_underline
+            assert not row[1].is_strikethrough
+            assert row[1].font_size == 12.0
+            assert row[1].font_name == "Arial"
+            assert row[2].font_color == (0, 0, 0)
+
+            assert row[2].style_name == "CustomStyle2"
+            assert not row[2].is_bold
+            assert not row[2].is_italic
+            assert row[2].is_underline
+            assert not row[2].is_strikethrough
+            assert row[2].font_size == 13.0
+            assert row[2].font_name == "Impact"
+            assert row[2].font_color == (0, 0, 0)
+
+            assert row[3].style_name == "CustomStyle3"
+            assert not row[3].is_bold
+            assert not row[3].is_italic
+            assert not row[3].is_underline
+            assert row[3].is_strikethrough
+            assert row[3].font_size == 10.0
+            assert row[3].font_name == "Menlo"
+            assert row[3].font_color == (29, 177, 0)
+        elif row[0].value == "Fonts":
+            for cell in row[1:]:
+                if not isinstance(cell, EmptyCell):
+                    assert cell.font_name == cell.value
+
+    # assert table.cell("A3").style_name == "Heading Red"
+    # assert table.cell("B2").style_name == "ArialBold14"
+    # assert table.cell("C2").style_name == "CourierNew12"
+    # assert table.cell("B2").is_bold
+    # assert not table.cell("B3").is_bold
+    # assert table.cell("C2").is_italic
+    # assert table.cell("D2").font_color == (29, 177, 0)
+    # assert table.cell("B2").font_size == 14.0
+    # assert table.cell("C2").font_size == 12.0
+    # assert table.cell("D2").font_size == 11.0
+    # assert table.cell("B2").font_name == "Arial-Black"
+    # assert table.cell("C2").font_name == "CourierNewPSMT"
+    # assert table.cell("D2").font_name == "HelveticaNeue"
