027abfe8903dccb6bf3a1ce70220c643ddb1d7d5
Jon Connell <python@figsandfudge.com>
2023-07-23 19:38:59 +0100

    Migrate to pytest

--- tests/test_borders.py
+++ tests/test_borders.py
@@ -1,9 +1,7 @@
-from numbers_parser import Document, MergedCell
-from numbers_parser.cell import Border
+from pytest_check import check
 
-doc = Document("tests/data/test-styles.numbers")
-table = doc.sheets["Large Borders"].tables[0]
-strokes = doc._model.extract_strokes(table._table_id)
+from numbers_parser import Document, MergedCell
+from numbers_parser.cell import Border, xl_rowcol_to_cell
 
 
 def check_border(border, test_value):
@@ -12,28 +10,57 @@ def check_border(border, test_value):
     values[1] = eval(values[1].replace(";", ","))
     if border is None:
         return False
-    return border == Border(values[0], values[1], values[2])
+    return check.equal(border, Border(values[0], values[1], values[2]))
 
 
-for row_num, row in enumerate(table.iter_rows()):
-    for col_num, cell in enumerate(row):
-        if isinstance(cell, MergedCell):
-            continue
+def print_borders(table):
+    for row_num, row in enumerate(table.iter_rows()):
+        for col_num, cell in enumerate(row):
+            if isinstance(cell, MergedCell):
+                continue
 
-        tests = cell.value.split("\n")
-        if all(
-            [
-                check_border(cell._border.top, tests[0][2:]),
-                check_border(cell._border.left, tests[1][2:]),
-                check_border(cell._border.right, tests[2][2:]),
-                check_border(cell._border.bottom, tests[3][2:]),
-            ]
-        ):
-            print(f"@[{row_num},{col_num}]: OK")
-        else:
-            print(f"@[{row_num},{col_num}]: FAIL")
-            print("   reference :", ", ".join(tests))
+            if all(
+                [
+                    x is None
+                    for x in [
+                        cell._border.top,
+                        cell._border.left,
+                        cell._border.right,
+                        cell._border.bottom,
+                    ]
+                ]
+            ):
+                continue
+
+            print(f"@[{row_num},{col_num}]:")
             print("   top       :", cell._border.top)
-            print("   left      :", cell._border.left)
             print("   right     :", cell._border.right)
             print("   bottom    :", cell._border.bottom)
+            print("   left      :", cell._border.left)
+
+
+def test_borders():
+    doc = Document("tests/data/test-styles.numbers")
+    table = doc.sheets["Large Borders"].tables[0]
+    strokes = doc._model.extract_strokes(table._table_id)
+
+    for row_num, row in enumerate(table.iter_rows()):
+        for col_num, cell in enumerate(row):
+            if isinstance(cell, MergedCell):
+                continue
+
+            tests = cell.value.split("\n")
+            valid = [
+                check_border(cell._border.top, tests[0][2:]),
+                check_border(cell._border.right, tests[1][2:]),
+                check_border(cell._border.bottom, tests[2][2:]),
+                check_border(cell._border.left, tests[3][2:]),
+            ]
+            if not all(valid):
+                print(f"@[{xl_rowcol_to_cell(row_num, col_num)}]: FAIL {valid}")
+                print("   reference :", ", ".join(tests))
+                print("   top       :", cell._border.top)
+                print("   right     :", cell._border.right)
+                print("   bottom    :", cell._border.bottom)
+                print("   left      :", cell._border.left)
+            assert valid
