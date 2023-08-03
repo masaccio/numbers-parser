e40a7f63a9eb28be6072ecff9fa4d964b0f9c9d3
Jon Connell <python@figsandfudge.com>
2023-08-03 19:27:48 +0100

    Initial commit

---
+++ src/debug/dump_formats.py
@@ -0,0 +1,29 @@
+import argparse
+
+from numbers_parser import Document
+
+
+def dump_format(format):
+    pass
+
+
+def dump_custom_formats_for_doc(doc):
+    objects = doc._model.objects
+    custom_format_id = doc._model.find_refs("CustomFormatListArchive")[0]
+    custom_formats = objects[custom_format_id].custom_formats
+    for format in sorted(custom_formats, key=lambda x: x.name):
+        print(format)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "numbers",
+    nargs="*",
+    metavar="numbers-filename",
+    help="Numbers folders/files to dump",
+)
+args = parser.parse_args()
+
+for filename in args.numbers:
+    doc = Document(filename)
+    dump_custom_formats_for_doc(doc)
