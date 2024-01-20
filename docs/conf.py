bee409cf3b78be906cf25da5f08727b303da3d04
Jon Connell <python@figsandfudge.com>
2024-01-20 12:19:57 +0000

    Sphinx docs

--- docs/conf.py
+++ docs/conf.py
@@ -23,9 +23,23 @@ autodoc_default_options = {
     "autoclass_content": "class",
     "member-order": "bysource",
     "members": True,
-    "show-inheritance": True,
+    "show-inheritance": False,
 }
 
 sys.path.insert(0, os.path.abspath("../"))
 
-# extensions.append("sphinx_autodoc_typehints")
+
+def autodoc_skip_member(app, what, name, obj, skip, options):  # noqa: PLR0913
+    if skip:
+        return True
+    if what == "class":
+        if name in ["Table", "Sheet"]:
+            return True
+    elif what == "method":
+        if name in ["Table.__init__", "Sheet.__init__"]:
+            return True
+    return False
+
+
+def setup(app):
+    app.connect("autodoc-skip-member", autodoc_skip_member)
