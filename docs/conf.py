13605354597b9b4a3ec05ed8322f2ac8a7bb2eed
Jon Connell <python@figsandfudge.com>
2024-02-02 08:27:11 +0000

    Switch to nefertiti theme

--- docs/conf.py
+++ docs/conf.py
@@ -5,8 +5,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
     "sphinx.ext.napoleon",
-    "sphinxawesome_theme",
-    "sphinxawesome_theme.highlighting",
+    "sphinx_nefertiti",
     "enum_tools.autoenum",
 ]
 
@@ -24,7 +23,5 @@ napoleon_include_special_with_doc = False
 napoleon_use_admonition_for_examples = True
 napoleon_use_admonition_for_notes = True
 
-html_permalinks_icon = "<span>#</span>"
-html_theme = "sphinxawesome_theme"
-
+html_theme = "sphinx_nefertiti"
 sys.path.insert(0, os.path.abspath("../"))
