aa3548cdad5ba9bd3fa1be11b46a97e3e8eb9465
Jon Connell <python@figsandfudge.com>
2024-02-03 18:11:31 +0000

    Additional Nefertiti config

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+from numbers_parser import _get_version
+
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
@@ -14,7 +16,7 @@ exclude_patterns = ["build", "Thumbs.db", ".DS_Store"]
 language = "en"
 master_doc = "index"
 project = "numbers-parser"
-copyright = "Jon Connell"
+copyright = "Copyright Jon Connell under MIT license"
 
 # sphinx.ext.napoleon configuration
 napoleon_google_docstring = False
@@ -24,4 +26,33 @@ napoleon_use_admonition_for_examples = True
 napoleon_use_admonition_for_notes = True
 
 html_theme = "sphinx_nefertiti"
+html_style = [
+    "custom.css",
+]
+
 sys.path.insert(0, os.path.abspath("../"))
+
+html_theme = "sphinx_nefertiti"
+html_style = ["custom.css", "nftt-pygments.min.css"]
+pygments_style = "pastie"
+pygments_dark_style = "dracula"
+
+html_theme_options = {
+    # "documentation_font": "Open Sans",
+    # "monospace_font": "Ubuntu Mono",
+    # "monospace_font_size": "1.1rem",
+    # "logo": "docs/logo.svg",
+    # "logo_alt": "numbers-parser",
+    "repository_url": "https://github.com/masaccio/numbers-parser",
+    "repository_name": "masaccio/numbers-parser",
+    "current_version": _get_version(),
+    "footer_links": ",".join(
+        [
+            "Documentation|https://masaccio.github.io/numbers-parser/",
+            "Package|https://pypi.org/project/numbers-parser/",
+            "Repository|https://github.com/masaccio/numbers-parser",
+            "Issues|https://github.com/masaccio/numbers-parser/issues",
+        ]
+    ),
+    # "show_colorset_choices": True,
+}
