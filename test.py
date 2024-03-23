f17940ca053c967dbcfaec9b0fd88152b7e12fc0
Jon Connell <python@figsandfudge.com>
2024-03-23 13:53:51 +0000

    Test file deleted

--- test.py
+++
@@ -1,6 +0,0 @@
-from numbers_parser import Document
-
-doc = Document("/Users/jon/Downloads/test1.numbers")
-for row in range(doc.default_table.num_rows):
-    doc.default_table.write(row, 1, "TEST")
-doc.save("/Users/jon/Downloads/test2.numbers")
