5e8656a132236e4a39498fd0c2c6655e469772f9
Jon Connell <python@figsandfudge.com>
2025-02-19 16:57:40 +0000

    Copying from main branch

--- tests/test_roman.py
+++ tests/test_roman.py
@@ -1,18 +1,24 @@
 import pytest
-import roman
 
 from numbers_parser.roman import to_roman
 
 
-def test_roman():
-    for num in range(1, 4000):
-        assert to_roman(num) == roman.toRoman(num)
+def test_to_roman():
+    assert to_roman(1) == "I"
+    assert to_roman(4) == "IV"
+    assert to_roman(9) == "IX"
+    assert to_roman(40) == "XL"
+    assert to_roman(90) == "XC"
+    assert to_roman(400) == "CD"
+    assert to_roman(500) == "D"
+    assert to_roman(900) == "CM"
+    assert to_roman(1000) == "M"
+    assert to_roman(3999) == "MMMCMXCIX"
+    assert to_roman(0) == "N"
 
-    with pytest.raises(ValueError) as e:
+    with pytest.raises(ValueError, match="Number out of range for Roman numerals") as e:
         _ = to_roman(4000)
     assert "Number out of range for Roman numerals" in str(e)
     with pytest.raises(ValueError) as e:
         _ = to_roman(-1)
     assert "Number out of range for Roman numerals" in str(e)
-
-    assert to_roman(0) == "N"
