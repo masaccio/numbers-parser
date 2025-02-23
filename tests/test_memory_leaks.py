41dee3e1f457f808eeccf3af4205d1a50cbf0c7d
Jon Connell <python@figsandfudge.com>
2025-02-23 11:00:41 +0000

    Use pympler for memory leak test

--- tests/test_memory_leaks.py
+++ tests/test_memory_leaks.py
@@ -1,33 +1,27 @@
-import pytest
-from psutil import Process
+import gc
+
+from pympler import muppy, summary
 
 from numbers_parser import Document
 
-NUM_DOCS = 10
 
-
-@pytest.mark.experimental
-def test_mempory_leaks():
+def test_memory_leaks():
     """Memory leak test (see issue-67)."""
-    process = Process()
+    sum1 = summary.summarize(muppy.get_objects())
 
-    rss_base = process.memory_info().rss / 1000000.0
+    doc = Document("tests/data/issue-67.numbers")
+    del doc
+    gc.collect()
 
-    print()
-    print(f"LEAK TEST: Creating {NUM_DOCS} large documents")
-    print(f"LEAK TEST: RSS={rss_base:.1f}MB")
+    sum2 = summary.summarize(muppy.get_objects())
+    diff = summary.get_diff(sum1, sum2)
 
-    for i in range(NUM_DOCS):
-        doc = Document("tests/data/issue-67.numbers")
-        assert doc.sheets[0].tables[0].cell(0, 0).value == "A"
+    summary.print_(diff)
 
-        rss = process.memory_info().rss / 1000000.0
-        if i == int(NUM_DOCS / 2):
-            pass
-        print(f"LEAK TEST: iteration #{i}: RSS={rss:.1f}MB")
+    total_size = 0
+    for func in filter(lambda x: x[1] > 0, diff):
+        total_size += func[2]
 
-    rss_end = process.memory_info().rss / 1000000.0
+    total_size /= 1024 * 1024
 
-    print(f"LEAK TEST: Memory in use: base={rss_base:.1f}MB, now={rss_end:.1f}MB")
-
-    assert rss_end < rss_base * 5
+    assert total_size < 2.0
