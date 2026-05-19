f7807bd373d233108ba2243a5675da7a20311227
Jon Connell <python@figsandfudge.com>
2026-05-19 11:18:27 +0100

    Make memory leak test always run

--- tests/test_memory_leaks.py
+++ tests/test_memory_leaks.py
@@ -1,28 +1,27 @@
 import gc
 
-import pytest
 from pympler import muppy, summary
 
 from numbers_parser import Document
 
-@pytest.mark.experimental
+
 def test_memory_leaks():
     """Memory leak test (see issue-67)."""
-    sum1 = summary.summarize(muppy.get_objects())
+    last_num_objects = None
+    last_num_bytes = None
+    iterations = 10
+    for ii in range(iterations):
+        doc = Document("tests/data/test-1.numbers")
+        del doc
+        gc.collect()
 
-    doc = Document("tests/data/issue-67.numbers")
-    del doc
-    gc.collect()
+        interim_summary = summary.summarize(muppy.get_objects())
+        num_objects = sum(r[1] for r in interim_summary)
+        num_bytes = sum(r[2] for r in interim_summary)
 
-    sum2 = summary.summarize(muppy.get_objects())
-    diff = summary.get_diff(sum1, sum2)
+        if ii > 1:
+            assert num_objects - last_num_objects <= 0
+            assert num_bytes - last_num_bytes <= 0
 
-    summary.print_(diff)
-
-    total_size = 0
-    for func in filter(lambda x: x[1] > 0, diff):
-        total_size += func[2]
-
-    total_size /= 1024 * 1024
-
-    assert total_size < 2.0
+        last_num_objects = num_objects
+        last_num_bytes = num_bytes
