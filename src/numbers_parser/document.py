dfd8d685b3917c09df64344ef23ea9df85fe9f95
Jon Connell <python@figsandfudge.com>
2026-09-18 13:08:41 +0200

    Additional coverage

--- src/numbers_parser/document.py
+++ src/numbers_parser/document.py
@@ -1007,8 +1007,6 @@ class Table(Cacheable):
     def set_cell_style(self, *args) -> None:
         (row, col, style) = self._validate_cell_coords(*args)
         if isinstance(style, Style):
-            if style.name is None:
-                style.name = self._model.custom_style_name()
             self._model.styles[style.name] = style
             self._data[row][col]._style = style
         elif isinstance(style, str):
